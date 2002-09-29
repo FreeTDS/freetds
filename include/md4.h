9a9c152aa63078de731a1527dd88d6d469646d7e
Steve Langasek <vorlon@dodds.net>
2002-09-29 07:51:15 +0000

    Include an implementation of MD4 in libtds, to begin moving away from the dependency on OpenSSL

---
+++ include/md4.h
@@ -0,0 +1,18 @@
+#ifndef MD4_H
+#define MD4_H
+
+struct MD4Context {
+	TDS_UINT buf[4];
+	TDS_UINT bits[2];
+	unsigned char in[64];
+};
+
+void MD4Init(struct MD4Context *context);
+void MD4Update(struct MD4Context *context, unsigned char const *buf,
+	       unsigned len);
+void MD4Final( struct MD4Context *context, unsigned char *digest);
+void MD4Transform(TDS_UINT buf[4], TDS_UINT const in[16]);
+
+typedef struct MD4Context MD4_CTX;
+
+#endif /* !MD4_H */
