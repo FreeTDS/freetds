c3a61b84c1c870f1f5d3cb256eef5e0c5155090c
Nicholas S. Castellano <entropy@freetds.org>
2002-10-19 18:03:13 +0000

    Add CVS Id.

--- include/tdsstring.h
+++ include/tdsstring.h
@@ -1,5 +1,31 @@
+/* FreeTDS - Library of routines accessing Sybase and Microsoft databases
+ * Copyright (C) 1998-1999  Brian Bruns
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #ifndef _tdsstring_h_
 #define _tdsstring_h_
+
+static char rcsid_tdsstring_h[] =
+	"$Id: tdsstring.h,v 1.5 2002-10-19 18:03:14 castellano Exp $";
+static void *no_unused_tdsstring_h_warn[] = {
+	rcsid_tdsstring_h,
+	no_unused_tdsstring_h_warn
+};
 
 extern char tds_str_empty[];
 
