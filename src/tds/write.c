c1f9f75c4bec319096b405657472ac2f4e922ec2
Frediano Ziglio <freddy77@gmail.com>
2002-12-02 21:05:12 +0000

    indent(1)

--- src/tds/write.c
+++ src/tds/write.c
@@ -53,16 +53,15 @@
 #endif /* HAVE_UNISTD_H */
 
 #include "tds.h"
-#include <signal.h> /* GW ADDED */
+#include <signal.h>		/* GW ADDED */
 #ifdef DMALLOC
 #include <dmalloc.h>
 #endif
 
-static char software_version[] =
-	"$Id: write.c,v 1.27 2002-11-24 10:22:36 freddy77 Exp $";
+static char software_version[] = "$Id: write.c,v 1.28 2002-12-02 21:05:12 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
-static int tds_write_packet(TDSSOCKET *tds, unsigned char final);
+static int tds_write_packet(TDSSOCKET * tds, unsigned char final);
 
 /** \addtogroup network
  *  \@{ 
@@ -73,15 +72,16 @@ static int tds_write_packet(TDSSOCKET *tds, unsigned char final);
  *		much like read() and memcpy()
  */
 int
-tds_put_n(TDSSOCKET *tds, const unsigned char *buf, int n)
+tds_put_n(TDSSOCKET * tds, const unsigned char *buf, int n)
 {
-int i;
-const unsigned char *bufp = buf;
-	if (bufp)  {
-		for (i=0;i<n;i++)	
+	int i;
+	const unsigned char *bufp = buf;
+
+	if (bufp) {
+		for (i = 0; i < n; i++)
 			tds_put_byte(tds, bufp[i]);
 	} else {
-		for (i=0;i<n;i++)	
+		for (i = 0; i < n; i++)
 			tds_put_byte(tds, '\0');
 	}
 	return 0;
@@ -93,42 +93,46 @@ const unsigned char *bufp = buf;
  * @param s   string to write
  * @param len lenth of string or -1 for null terminated
  */
-int 
-tds_put_string(TDSSOCKET *tds, const char *s, int len)
+int
+tds_put_string(TDSSOCKET * tds, const char *s, int len)
 {
-int res;
-char temp[256];
-const char *p;
-unsigned int bytes_left;
-	
-	if (len < 0) len = strlen(s);
+	int res;
+	char temp[256];
+	const char *p;
+	unsigned int bytes_left;
+
+	if (len < 0)
+		len = strlen(s);
 	if (IS_TDS7_PLUS(tds)) {
 		p = s;
 		res = 0;
-		while(len > 0) {
-			bytes_left = len > (sizeof(temp)/2) ? (sizeof(temp)/2) : len;
-			tds7_ascii2unicode(tds, p, temp, bytes_left*2);
-			res = tds_put_n(tds, temp, bytes_left*2);
+		while (len > 0) {
+			bytes_left = len > (sizeof(temp) / 2) ? (sizeof(temp) / 2) : len;
+			tds7_ascii2unicode(tds, p, temp, bytes_left * 2);
+			res = tds_put_n(tds, temp, bytes_left * 2);
 			p += bytes_left;
 			len -= bytes_left;
 		}
 		return res;
-		
+
 	}
-	return tds_put_n(tds,s,len);
+	return tds_put_n(tds, s, len);
 }
 
-int tds_put_buf(TDSSOCKET *tds, const unsigned char *buf, int dsize, int ssize)
+int
+tds_put_buf(TDSSOCKET * tds, const unsigned char *buf, int dsize, int ssize)
 {
-int  cpsize;
+	int cpsize;
 
 	cpsize = ssize > dsize ? dsize : ssize;
-	tds_put_n(tds,buf,cpsize);
+	tds_put_n(tds, buf, cpsize);
 	dsize -= cpsize;
-	tds_put_n(tds,NULL,dsize);
-	return tds_put_byte(tds,cpsize);
+	tds_put_n(tds, NULL, dsize);
+	return tds_put_byte(tds, cpsize);
 }
-int tds_put_int(TDSSOCKET *tds, TDS_INT i)
+
+int
+tds_put_int(TDSSOCKET * tds, TDS_INT i)
 {
 #if WORDS_BIGENDIAN
 	if (tds->emul_little_endian) {
@@ -139,9 +143,11 @@ int tds_put_int(TDSSOCKET *tds, TDS_INT i)
 		return 0;
 	}
 #endif
-	return tds_put_n(tds,(const unsigned char *)&i,sizeof(TDS_INT));
+	return tds_put_n(tds, (const unsigned char *) &i, sizeof(TDS_INT));
 }
-int tds_put_smallint(TDSSOCKET *tds, TDS_SMALLINT si)
+
+int
+tds_put_smallint(TDSSOCKET * tds, TDS_SMALLINT si)
 {
 #if WORDS_BIGENDIAN
 	if (tds->emul_little_endian) {
@@ -150,80 +156,90 @@ int tds_put_smallint(TDSSOCKET *tds, TDS_SMALLINT si)
 		return 0;
 	}
 #endif
-	return tds_put_n(tds,(const unsigned char *)&si,sizeof(TDS_SMALLINT));
+	return tds_put_n(tds, (const unsigned char *) &si, sizeof(TDS_SMALLINT));
 }
-int tds_put_tinyint(TDSSOCKET *tds, TDS_TINYINT ti)
+
+int
+tds_put_tinyint(TDSSOCKET * tds, TDS_TINYINT ti)
 {
-	return tds_put_byte(tds,(unsigned char)ti);
+	return tds_put_byte(tds, (unsigned char) ti);
 }
-int tds_put_byte(TDSSOCKET *tds, unsigned char c)
+
+int
+tds_put_byte(TDSSOCKET * tds, unsigned char c)
 {
 	if (tds->out_pos >= tds->env->block_size) {
-		tds_write_packet(tds,0x0);
+		tds_write_packet(tds, 0x0);
 		tds_init_write_buf(tds);
 	}
-	tds->out_buf[tds->out_pos++]=c;
+	tds->out_buf[tds->out_pos++] = c;
 	return 0;
 }
-int tds_put_bulk_data(TDSSOCKET *tds, const unsigned char * buf, TDS_INT bufsize)
+
+int
+tds_put_bulk_data(TDSSOCKET * tds, const unsigned char *buf, TDS_INT bufsize)
 {
 
 	tds->out_flag = 0x07;
-	return tds_put_n(tds,buf,bufsize);
+	return tds_put_n(tds, buf, bufsize);
 }
-int tds_init_write_buf(TDSSOCKET *tds)
+
+int
+tds_init_write_buf(TDSSOCKET * tds)
 {
-	memset(tds->out_buf,'\0',tds->env->block_size);
-	tds->out_pos=8;
+	memset(tds->out_buf, '\0', tds->env->block_size);
+	tds->out_pos = 8;
 	return 0;
 }
 
 /* TODO this code should be similar to read one... */
-static int 
-tds_check_socket_write(TDSSOCKET *tds)
+static int
+tds_check_socket_write(TDSSOCKET * tds)
 {
 int retcode = 0;
 struct timeval selecttimeout;
 time_t start, now;
 fd_set fds;
- 
+
 	/* Jeffs hack *** START OF NEW CODE */
-	FD_ZERO (&fds);
+	FD_ZERO(&fds);
 
 	if (!tds->timeout) {
-		for(;;) {
-			FD_SET (tds->s, &fds);
-			retcode = select (tds->s + 1, NULL, &fds, NULL, NULL);
+		for (;;) {
+			FD_SET(tds->s, &fds);
+			retcode = select(tds->s + 1, NULL, &fds, NULL, NULL);
 			/* write available */
-			if (retcode >= 0) return 0;
+			if (retcode >= 0)
+				return 0;
 			/* interrupted */
-			if (errno == EINTR) continue;
-			/* error, leave caller handle problems*/
+			if (errno == EINTR)
+				continue;
+			/* error, leave caller handle problems */
 			return -1;
 		}
 	}
 	start = time(NULL);
 	now = start;
- 
-	while ((retcode == 0) && ((now-start) < tds->timeout)) {
-		FD_SET (tds->s, &fds);
-		selecttimeout.tv_sec = tds->timeout - (now-start);
+
+	while ((retcode == 0) && ((now - start) < tds->timeout)) {
+		FD_SET(tds->s, &fds);
+		selecttimeout.tv_sec = tds->timeout - (now - start);
 		selecttimeout.tv_usec = 0;
-		retcode = select (tds->s + 1, NULL, &fds, NULL, &selecttimeout);
+		retcode = select(tds->s + 1, NULL, &fds, NULL, &selecttimeout);
 		if (retcode < 0 && errno == EINTR) {
 			retcode = 0;
 		}
 
-		now = time (NULL);
+		now = time(NULL);
 	}
- 
+
 	return retcode;
 	/* Jeffs hack *** END OF NEW CODE */
 }
 
 /* goodwrite function adapted from patch by freddy77 */
 static int
-goodwrite(TDSSOCKET *tds)
+goodwrite(TDSSOCKET * tds)
 {
 int left;
 char *p;
@@ -232,20 +248,21 @@ int retval;
 
 	left = tds->out_pos;
 	p = tds->out_buf;
-  
+
 	while (left > 0) {
 		/* If there's a timeout, we need to sit and wait for socket */
 		/* writability */
 		/* moved socket writability check to own function -- bsb */
 		tds_check_socket_write(tds);
 
-		retval = WRITESOCKET(tds->s,p,left);
+		retval = WRITESOCKET(tds->s, p, left);
 
 		if (retval <= 0) {
-			tdsdump_log(TDS_DBG_NETWORK, "TDS: Write failed in tds_write_packet\nError: %d (%s)\n", errno, strerror(errno));
+			tdsdump_log(TDS_DBG_NETWORK, "TDS: Write failed in tds_write_packet\nError: %d (%s)\n", errno,
+				    strerror(errno));
 			tds_client_msg(tds->tds_ctx, tds, 20006, 9, 0, 0, "Write to SQL Server failed.");
-			tds->in_pos=0;
-			tds->in_len=0;
+			tds->in_pos = 0;
+			tds->in_len = 0;
 			tds_close_socket(tds);
 			result = TDS_FAIL;
 			break;
@@ -256,30 +273,30 @@ int retval;
 	return result;
 }
 
-static int 
-tds_write_packet(TDSSOCKET *tds, unsigned char final)
+static int
+tds_write_packet(TDSSOCKET * tds, unsigned char final)
 {
-int retcode;
-void (*oldsig)(int);
+	int retcode;
+	void (*oldsig) (int);
 
-	tds->out_buf[0]=tds->out_flag;
-	tds->out_buf[1]=final;
-	tds->out_buf[2]=(tds->out_pos)/256u;
-	tds->out_buf[3]=(tds->out_pos)%256u;
+	tds->out_buf[0] = tds->out_flag;
+	tds->out_buf[1] = final;
+	tds->out_buf[2] = (tds->out_pos) / 256u;
+	tds->out_buf[3] = (tds->out_pos) % 256u;
 	if (IS_TDS70(tds) || IS_TDS80(tds)) {
-		tds->out_buf[6]=0x01;
+		tds->out_buf[6] = 0x01;
 	}
 
 	tdsdump_log(TDS_DBG_NETWORK, "Sending packet @ %L\n%D\n", tds->out_buf, tds->out_pos);
 
-	oldsig=signal (SIGPIPE, SIG_IGN);
-	if (oldsig==SIG_ERR) {
+	oldsig = signal(SIGPIPE, SIG_IGN);
+	if (oldsig == SIG_ERR) {
 		tdsdump_log(TDS_DBG_WARN, "TDS: Warning: Couldn't set SIGPIPE signal to be ignored\n");
 	}
 
 	retcode = goodwrite(tds);
 
-	if (signal(SIGPIPE, oldsig)==SIG_ERR) {
+	if (signal(SIGPIPE, oldsig) == SIG_ERR) {
 		tdsdump_log(TDS_DBG_WARN, "TDS: Warning: Couldn't reset SIGPIPE signal to previous value\n");
 	}
 
@@ -292,15 +309,16 @@ void (*oldsig)(int);
  * @return TDS_FAIL or TDS_SUCCEED
  */
 int
-tds_flush_packet(TDSSOCKET *tds)
+tds_flush_packet(TDSSOCKET * tds)
 {
 int result = TDS_FAIL;
 
 	/* GW added check for tds->s */
 	if (!IS_TDSDEAD(tds)) {
-		result = tds_write_packet(tds,0x01);
+		result = tds_write_packet(tds, 0x01);
 		tds_init_write_buf(tds);
 	}
 	return result;
 }
+
 /** \@} */
