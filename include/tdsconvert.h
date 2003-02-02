cfed28ee70fbb9505081de8cacaba11947718a53
Frediano Ziglio <freddy77@gmail.com>
2003-02-02 09:27:05 +0000

    fix declarationusing c++

--- include/tdsconvert.h
+++ include/tdsconvert.h
@@ -20,28 +20,35 @@
 #ifndef _tdsconvert_h_
 #define _tdsconvert_h_
 
-static char rcsid_tdsconvert_h[] =
-         "$Id: tdsconvert.h,v 1.16 2002-11-17 10:01:03 freddy77 Exp $";
-static void *no_unused_tdsconvert_h_warn[] = {
-	rcsid_tdsconvert_h, 
-	no_unused_tdsconvert_h_warn};
+#ifdef __cplusplus
+extern "C"
+{
+#if 0
+}
+#endif
+#endif
 
-typedef union conv_result {
-    TDS_TINYINT     ti;
-    TDS_SMALLINT    si;
-    TDS_INT         i;
-    TDS_INT8        bi;
-    TDS_FLOAT       f;
-    TDS_REAL        r;
-    TDS_CHAR        *c;
-    TDS_MONEY       m;
-    TDS_MONEY4      m4;
-    TDS_DATETIME    dt;
-    TDS_DATETIME4   dt4;
-    TDS_NUMERIC     n;
-    TDS_CHAR        *ib;
-    TDS_UNIQUE      u;
-} CONV_RESULT;
+static char rcsid_tdsconvert_h[] = "$Id: tdsconvert.h,v 1.17 2003-02-02 09:27:05 freddy77 Exp $";
+static void *no_unused_tdsconvert_h_warn[] = { rcsid_tdsconvert_h, no_unused_tdsconvert_h_warn };
+
+typedef union conv_result
+{
+	TDS_TINYINT ti;
+	TDS_SMALLINT si;
+	TDS_INT i;
+	TDS_INT8 bi;
+	TDS_FLOAT f;
+	TDS_REAL r;
+	TDS_CHAR *c;
+	TDS_MONEY m;
+	TDS_MONEY4 m4;
+	TDS_DATETIME dt;
+	TDS_DATETIME4 dt4;
+	TDS_NUMERIC n;
+	TDS_CHAR *ib;
+	TDS_UNIQUE u;
+}
+CONV_RESULT;
 
 /*
  * Failure return codes for tds_convert()
@@ -52,17 +59,19 @@ typedef union conv_result {
 #define TDS_CONVERT_NOMEM	-4	/* insufficient memory */
 #define TDS_CONVERT_OVERFLOW	-5	/* result too large */
 
-struct tds_time {
-int tm_year;
-int tm_mon;
-int tm_mday;
-int tm_hour;
-int tm_min;
-int tm_sec;
-int tm_ms;
+struct tds_time
+{
+	int tm_year;
+	int tm_mon;
+	int tm_mday;
+	int tm_hour;
+	int tm_min;
+	int tm_sec;
+	int tm_ms;
 };
 
-struct tds_tm {
+struct tds_tm
+{
 	struct tm tm;
 	int milliseconds;
 };
@@ -71,10 +80,15 @@ unsigned char tds_willconvert(int srctype, int desttype);
 
 TDS_INT tds_get_null_type(int srctype);
 int tds_get_conversion_type(int srctype, int colsize);
-TDS_INT tds_convert(TDSCONTEXT *context, int srctype, const TDS_CHAR *src, 
-		TDS_UINT srclen, int desttype, CONV_RESULT *cr);
+TDS_INT tds_convert(TDSCONTEXT * context, int srctype, const TDS_CHAR * src, TDS_UINT srclen, int desttype, CONV_RESULT * cr);
 
-size_t tds_strftime(char *buf, size_t maxsize, const char *format, const TDSDATEREC *timeptr);
+size_t tds_strftime(char *buf, size_t maxsize, const char *format, const TDSDATEREC * timeptr);
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
 
 #endif /* _tdsconvert_h_ */
-
