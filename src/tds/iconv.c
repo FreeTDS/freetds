595c705a437e8b0159174a7daceea280bee99564
Frediano Ziglio <freddy77@gmail.com>
2003-03-26 21:17:07 +0000

    ODBC fix and iconv merge

--- src/tds/iconv.c
+++ src/tds/iconv.c
@@ -42,7 +42,7 @@
 #include <dmalloc.h>
 #endif
 
-static char software_version[] = "$Id: iconv.c,v 1.40 2003-03-26 16:20:50 freddy77 Exp $";
+static char software_version[] = "$Id: iconv.c,v 1.41 2003-03-26 21:17:27 freddy77 Exp $";
 static void *no_unused_var_warn[] = {
 	software_version,
 	no_unused_var_warn
@@ -109,6 +109,202 @@ TDSICONVINFO *iconv_info;
 	}
 	if (iconv_info->cdfrom_srv != (iconv_t) - 1) {
 		iconv_close(iconv_info->cdfrom_srv);
+	}
+#endif
+}
+
+void
+tds7_srv_charset_changed(TDSSOCKET * tds, int lcid)
+{
+#if HAVE_ICONV
+	char *cp;
+	TDSICONVINFO *iconv_info;
+	iconv_t tmp_cd;
+
+	iconv_info = (TDSICONVINFO *) tds->iconv_info;
+
+	/* The table from the MSQLServer reference "Windows Collation Designators" 
+	 * and from " NLS Information for Microsoft Windows XP"
+	 */
+	switch (lcid) {
+	case 0x405:
+	case 0x40e:
+	case 0x415:
+	case 0x418:
+	case 0x41a:
+	case 0x41b:
+	case 0x41c:
+	case 0x424:
+	/* case 0x81a: */ /* seem wrong in XP table TODO check */
+	case 0x104e:		/* ?? */
+		cp = "CP1250";
+		break;
+	case 0x402:
+	case 0x419:
+	case 0x422:
+	case 0x423:
+	case 0x42f:
+	case 0x43f:
+	case 0x440:
+	case 0x444:
+	case 0x450:
+	case 0x81a:		/* ?? */
+	case 0x82c:
+	case 0x843:
+	case 0xc1a:
+		cp = "CP1251";
+		break;
+	case 0x1007:
+	case 0x1009:
+	case 0x100a:
+	case 0x100c:
+	case 0x1407:
+	case 0x1409:
+	case 0x140a:
+	case 0x140c:
+	case 0x10407:
+	case 0x10437:
+	case 0x1809:
+	case 0x180a:
+	case 0x180c:
+	case 0x1c09:
+	case 0x1c0a:
+	case 0x2009:
+	case 0x200a:
+	case 0x2409:
+	case 0x240a:
+	case 0x2809:
+	case 0x280a:
+	case 0x2c09:
+	case 0x2c0a:
+	case 0x3009:
+	case 0x300a:
+	case 0x3409:
+	case 0x340a:
+	case 0x380a:
+	case 0x3c0a:
+	case 0x400a:
+	case 0x403:
+	case 0x406:
+	case 0x407:
+	case 0x409:
+	case 0x40a:
+	case 0x40b:
+	case 0x40c:
+	case 0x40f:
+	case 0x410:
+	case 0x413:
+	case 0x414:
+	case 0x416:
+	case 0x41d:
+	case 0x421:
+	case 0x42d:
+	case 0x436:
+	case 0x438:
+/*case 0x439:  *//*??? Unicode only */
+	case 0x43e:
+	case 0x440a:
+	case 0x441:
+	case 0x456:
+	case 0x480a:
+	case 0x4c0a:
+	case 0x500a:
+	case 0x807:
+	case 0x809:
+	case 0x80a:
+	case 0x80c:
+	case 0x810:
+	case 0x813:
+	case 0x814:
+	case 0x816:
+	case 0x81d:
+	case 0x83e:
+	case 0xc07:
+	case 0xc09:
+	case 0xc0a:
+	case 0xc0c:
+		cp = "CP1252";
+		break;
+	case 0x408:
+		cp = "CP1253";
+		break;
+	case 0x41f:
+	case 0x42c:
+	case 0x443:
+		cp = "CP1254";
+		break;
+	case 0x40d:
+		cp = "CP1255";
+		break;
+	case 0x1001:
+	case 0x1401:
+	case 0x1801:
+	case 0x1c01:
+	case 0x2001:
+	case 0x2401:
+	case 0x2801:
+	case 0x2c01:
+	case 0x3001:
+	case 0x3401:
+	case 0x3801:
+	case 0x3c01:
+	case 0x4001:
+	case 0x401:
+	case 0x420:
+	case 0x429:
+	case 0x801:
+	case 0xc01:
+		cp = "CP1256";
+		break;
+	case 0x425:
+	case 0x426:
+	case 0x427:
+	case 0x827:		/* ?? */
+		cp = "CP1257";
+		break;
+	case 0x42a:
+		cp = "CP1258";
+		break;
+	case 0x41e:
+		cp = "CP874";
+		break;
+	case 0x411:
+	case 0x10411:
+		cp = "CP932";
+		break;
+	case 0x1004:
+	case 0x20804:
+	case 0x804:
+		cp = "CP936";
+		break;
+	case 0x412:
+		cp = "CP949";
+		break;
+	case 0x1404:
+	case 0x30404:
+	case 0x404:
+	case 0xc04:
+		cp = "CP950";
+		break;
+	default:
+		cp = "CP1252";
+	}
+
+	/* TODO use real client charset */
+	tmp_cd = iconv_open(cp, /*iconv_info->client_charset*/"");
+	if (tmp_cd != (iconv_t) - 1) {
+		if (iconv_info->cdto_srv != (iconv_t) - 1) {
+			iconv_close(iconv_info->cdto_srv);
+		}
+		iconv_info->cdto_srv = tmp_cd;
+	}
+
+	tmp_cd = iconv_open(/*iconv_info->client_charset*/"", cp);
+	if (tmp_cd != (iconv_t) - 1) {
+		if (iconv_info->cdfrom_srv != (iconv_t) - 1) {
+			iconv_close(iconv_info->cdfrom_srv);
+		}
+		iconv_info->cdfrom_srv = tmp_cd;
 	}
 #endif
 }
