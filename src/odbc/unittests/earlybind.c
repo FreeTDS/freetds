a96047e369870df57f3448ee4e112f109d029d40
Frediano Ziglio <freddy77@gmail.com>
2003-05-11 08:33:48 +0000

    did some odbc TODO

---
+++ src/odbc/unittests/earlybind.c
@@ -0,0 +1,72 @@
+#include "common.h"
+
+static char software_version[] = "$Id: earlybind.c,v 1.1 2003-05-11 08:33:48 freddy77 Exp $";
+static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
+
+int
+main(int argc, char *argv[])
+{
+	SQLINTEGER id, ind1, ind2;
+	char name[64];
+
+	Connect();
+
+	/* bind before select */
+	SQLBindCol(Statement, 1, SQL_C_SLONG, &id, sizeof(SQLINTEGER), &ind1);
+	SQLBindCol(Statement, 2, SQL_C_CHAR, name, sizeof(name), &ind2);
+
+	/* do select */
+	Command(Statement, "SELECT id, name FROM sysobjects WHERE name = 'sysobjects' SELECT 123, 'foo'");
+
+	/* get results */
+	id = -1;
+	memset(name, 0, sizeof(name));
+	SQLFetch(Statement);
+
+	if (id == -1 || strcmp(name, "sysobjects") != 0) {
+		fprintf(stderr, "wrong results\n");
+		return 1;
+	}
+
+	/* discard others data */
+	SQLFetch(Statement);
+
+	SQLMoreResults(Statement);
+
+	id = -1;
+	memset(name, 0, sizeof(name));
+	SQLFetch(Statement);
+
+	if (id != 123 || strcmp(name, "foo") != 0) {
+		fprintf(stderr, "wrong results\n");
+		return 1;
+	}
+
+	/* discard others data */
+	SQLFetch(Statement);
+
+	SQLMoreResults(Statement);
+
+	/* other select */
+	Command(Statement, "SELECT 321, 'minni'");
+
+	/* get results */
+	id = -1;
+	memset(name, 0, sizeof(name));
+	SQLFetch(Statement);
+
+	if (id != 321 || strcmp(name, "minni") != 0) {
+		fprintf(stderr, "wrong results\n");
+		return 1;
+	}
+
+	/* discard others data */
+	SQLFetch(Statement);
+
+	SQLMoreResults(Statement);
+
+	Disconnect();
+
+	printf("Done.\n");
+	return 0;
+}
