d4000ab94d51791c9674bc1074ef688af2183bef
Frediano Ziglio <freddy77@gmail.com>
2003-07-29 09:48:30 +0000

    update export files

---
+++ src/odbc/checkexport.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo Function not exported by VMS driver
+(nm .libs/libtdsodbc.so | sed 's,.* T ,,g'; cat ../../vms/odbc_driver_axp.opt | grep -v '^!' | grep 'SQL' | sed 's,.*SQL\(.*\)=PROCEDURE.*,SQL\1,g') | sort | uniq -u
+
+echo Function not exported by windows driver
+(nm .libs/libtdsodbc.so | sed 's,.* T ,,g'; cat ../../win32/FreeTDS.def | grep -v '^;' | grep 'SQL' | sed 's,.*SQL,SQL,g' | perl -pe 's,\r,,g') | sort | uniq -u
