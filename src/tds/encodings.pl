c8ca46482e8561cb8f11664aa41fe7e42e918752
James K. Lowden <jklowden@freetds.org>
2003-09-18 20:25:55 +0000

    cannot look up charset '646' properly, attempted to fix off-by-one in src/tds/encodings.h, but ran out of time.

--- src/tds/encodings.pl
+++ src/tds/encodings.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 
 print "/*\n";
-print " * This file produced from $0\n";
-print ' * $Id: encodings.pl,v 1.4 2003-09-17 07:31:15 freddy77 Exp $', "\n";
+$date = localtime;
+print " * This file produced from $0 on $date\n";
+print ' * $Id: encodings.pl,v 1.5 2003-09-18 20:25:55 jklowden Exp $', "\n";
 print " */\n";
 
 %charsets = ();
@@ -49,15 +50,19 @@ $index{"UCS-2LE"} = $i++;
 $index{"UCS-2BE"} = $i++;
 foreach $n (sort grep(!/^(ISO-8859-1|UTF-8|UCS-2LE|UCS-2BE)$/,keys %charsets))
 {
-	$index{$n} = $i++;
+	$index{$n} = $i;
+	$i++ if $n;
 }
 
 # output all charset
 print "static const TDS_ENCODING canonic_charsets[] = {\n";
+$i=0;
 foreach $n (sort { $index{$a} <=> $index{$b} } keys %charsets)
 {
-	($a, $b) = @{$charsets{$n}};
-	print qq|\t{"$n",\t$a, $b},\n|;
+	my ($a, $b) = @{$charsets{$n}};
+	my $index = ($n)? (sprintf "/* %3d */", $i) : "\t";
+	printf "\t{%20s,\t$a, $b},\t%s\n", qq/"$n"/, $index;
+	$i++ if $n;
 }
 print "\t{\"\",\t0, 0}\n";
 print "};\n\n";
@@ -68,7 +73,7 @@ foreach $n (sort keys %alternates)
 {
 	$a = $index{$alternates{$n}};
 	next if ("$a" eq "");
-	print qq|\t{"$n",\t$a},\n|;
+	printf "\t{%25s,  %3d },\n", qq/"$n"/, $a;
 }
 print "\t{NULL,\t0}\n";
 print "};\n\n";
@@ -79,7 +84,8 @@ foreach $n (sort keys %sybase)
 {
 	$a = $index{$sybase{$n}};
 	next if ("$a" eq "");
-	print qq|\t{"$n",\t$a},\n|;
+	printf "\t{%20s, %3d },\n", qq/"$n"/, $a;
+	#rint qq|\t{"$n",\t$a},\n|;
 }
 print "\t{NULL,\t0}\n";
 print "};\n\n";
