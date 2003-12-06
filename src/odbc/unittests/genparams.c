1a2704b620cc923fb8b53a4c1b2e620b38cb5ec1
Frediano Ziglio <freddy77@gmail.com>
2003-12-06 16:35:16 +0000

    improve test for Ruby problem

--- src/odbc/unittests/genparams.c
+++ src/odbc/unittests/genparams.c
@@ -2,7 +2,7 @@
 
 /* Test various type from odbc and to odbc */
 
-static char software_version[] = "$Id: genparams.c,v 1.2 2003-11-13 13:52:53 jklowden Exp $";
+static char software_version[] = "$Id: genparams.c,v 1.3 2003-12-06 16:35:17 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
 static void
@@ -14,6 +14,9 @@ Test(const char *type, const char *value_to_convert, SQLSMALLINT out_c_type, SQL
 	SQL_NUMERIC_STRUCT *num;
 	int i;
 
+	SQLFreeStmt(Statement, SQL_UNBIND);
+	SQLFreeStmt(Statement, SQL_RESET_PARAMS);
+
 	/* build store procedure to test */
 	sprintf(sbuf, "CREATE PROC spTestProc @i %s OUTPUT AS SELECT @i = CONVERT(%s, '%s')", type, type, value_to_convert);
 	Command(Statement, sbuf);
@@ -24,9 +27,9 @@ Test(const char *type, const char *value_to_convert, SQLSMALLINT out_c_type, SQL
 		fprintf(stderr, "Unable to bind input parameter\n");
 		CheckReturn();
 	}
-	
-		/* call store procedure */
-		if (SQLExecDirect(Statement, "{call spTestProc(?)}", SQL_NTS) != SQL_SUCCESS) {
+
+	/* call store procedure */
+	if (SQLExecDirect(Statement, "{call spTestProc(?)}", SQL_NTS) != SQL_SUCCESS) {
 		fprintf(stderr, "Unable to execute store statement\n");
 		CheckReturn();
 	}
@@ -51,6 +54,79 @@ Test(const char *type, const char *value_to_convert, SQLSMALLINT out_c_type, SQL
 	Command(Statement, "drop proc spTestProc");
 }
 
+static void
+TestInput(SQLSMALLINT out_c_type, const char *type, SQLSMALLINT out_sql_type, const char *param_type, const char *value_to_convert)
+{
+	char sbuf[1024];
+	unsigned char out_buf[256];
+	SQLINTEGER out_len = 0;
+
+	SQLFreeStmt(Statement, SQL_UNBIND);
+	SQLFreeStmt(Statement, SQL_RESET_PARAMS);
+
+	/* execute a select to get data as wire */
+	sprintf(sbuf, "SELECT CONVERT(%s, '%s')", type, value_to_convert);
+	Command(Statement, sbuf);
+	SQLBindCol(Statement, 1, out_c_type, out_buf, sizeof(SQLINTEGER), &out_len);
+	if (SQLFetch(Statement) != SQL_SUCCESS) {
+		fprintf(stderr, "Expected row\n");
+		exit(1);
+	}
+	if (SQLFetch(Statement) != SQL_NO_DATA) {
+		fprintf(stderr, "Row not expected\n");
+		exit(1);
+	}
+	if (SQLMoreResults(Statement) != SQL_NO_DATA) {
+		fprintf(stderr, "Recordset not expected\n");
+		exit(1);
+	}
+
+	/* create a table with a column of that type */
+	SQLFreeStmt(Statement, SQL_UNBIND);
+	SQLFreeStmt(Statement, SQL_RESET_PARAMS);
+	sprintf(sbuf, "CREATE TABLE #tmp_insert (col %s)", param_type);
+	Command(Statement, sbuf);
+
+	/* insert data using prepared statements */
+	sprintf(sbuf, "INSERT INTO #tmp_insert VALUES(?)");
+	if (SQLPrepare(Statement, sbuf, SQL_NTS) != SQL_SUCCESS) {
+		fprintf(stderr, "SQLPrepare() failure!\n");
+		exit(1);
+	}
+
+	out_len = 1;
+	if (SQLBindParameter(Statement, 1, SQL_PARAM_INPUT, out_c_type, out_sql_type, 20, 0, out_buf, sizeof(out_buf), &out_len) !=
+	    SQL_SUCCESS) {
+		fprintf(stderr, "Unable to bind input parameter\n");
+		CheckReturn();
+	}
+
+	if (SQLExecute(Statement) != SQL_SUCCESS) {
+		fprintf(stderr, "SQLExecute() failure!\n");
+		exit(1);
+	}
+
+	/* check is row is present */
+	SQLFreeStmt(Statement, SQL_UNBIND);
+	SQLFreeStmt(Statement, SQL_RESET_PARAMS);
+	sprintf(sbuf, "SELECT * FROM #tmp_insert WHERE col = CONVERT(%s, '%s')", param_type, value_to_convert);
+	Command(Statement, sbuf);
+
+	if (SQLFetch(Statement) != SQL_SUCCESS) {
+		fprintf(stderr, "Expected row\n");
+		exit(1);
+	}
+	if (SQLFetch(Statement) != SQL_NO_DATA) {
+		fprintf(stderr, "Row not expected\n");
+		exit(1);
+	}
+	if (SQLMoreResults(Statement) != SQL_NO_DATA) {
+		fprintf(stderr, "Recordset not expected\n");
+		exit(1);
+	}
+	Command(Statement, "DROP TABLE #tmp_insert");
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -61,6 +137,7 @@ main(int argc, char *argv[])
 
 	/* FIXME why should return 38 0 as precision and scale ?? correct ?? */
 	Test("NUMERIC(18,2)", "123", SQL_C_NUMERIC, SQL_NUMERIC, "18 0 1 7B");
+	TestInput(SQL_C_LONG, "INTEGER", SQL_VARCHAR, "VARCHAR(20)", "12345");
 
 	Disconnect();
 
