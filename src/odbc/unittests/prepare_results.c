db92a5bc9d5486cff9422eef4da76ad8f6d38d0f
Frediano Ziglio <freddy77@gmail.com>
2004-02-09 13:03:57 +0000

    new test for SQLPrepare

---
+++ src/odbc/unittests/prepare_results.c
@@ -0,0 +1,72 @@
+#include "common.h"
+
+/* Test for data format returned from SQLPrepare */
+
+static char software_version[] = "$Id: prepare_results.c,v 1.1 2004-02-09 13:04:05 freddy77 Exp $";
+static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
+
+int
+main(int argc, char *argv[])
+{
+	SQLSMALLINT count, namelen, type, digits, nullable;
+	SQLUINTEGER size;
+	char name[128];
+
+	Connect();
+
+	Command(Statement, "create table #odbctestdata (i int, c char(20), n numeric(34,12) )");
+
+	/* test query returns column information */
+	if (SQLPrepare(Statement, "select * from #odbctestdata select * from #odbctestdata", SQL_NTS) != SQL_SUCCESS) {
+		printf("SQLPrepare return failure\n");
+		exit(1);
+	}
+
+	if (SQLNumResultCols(Statement, &count) != SQL_SUCCESS) {
+		printf("SQLNumResultCols return failure\n");
+		exit(1);
+	}
+
+	if (count != 3) {
+		printf("Wrong number of columns returned. Got %d expected 2\n", (int) count);
+		exit(1);
+	}
+
+	if (SQLDescribeCol(Statement, 1, name, sizeof(name), &namelen, &type, &size, &digits, &nullable) != SQL_SUCCESS) {
+		printf("SQLDescribeCol failure for column 1\n");
+		exit(1);
+	}
+
+	if (type != SQL_INTEGER || strcmp(name, "i") != 0) {
+		printf("wrong column 1 informations\n");
+		exit(1);
+	}
+
+	if (SQLDescribeCol(Statement, 2, name, sizeof(name), &namelen, &type, &size, &digits, &nullable) != SQL_SUCCESS) {
+		printf("SQLDescribeCol failure for column 2\n");
+		exit(1);
+	}
+
+	if (type != SQL_CHAR || strcmp(name, "c") != 0 || size != 20) {
+		printf("wrong column 2 informations\n");
+		exit(1);
+	}
+
+	if (SQLDescribeCol(Statement, 3, name, sizeof(name), &namelen, &type, &size, &digits, &nullable) != SQL_SUCCESS) {
+		printf("SQLDescribeCol failure for column 3\n");
+		exit(1);
+	}
+
+	if (type != SQL_NUMERIC || strcmp(name, "n") != 0 || size != 34 || digits != 12) {
+		printf("wrong column 3 informations\n");
+		exit(1);
+	}
+
+	/* TODO test SQLDescribeParam (when implemented) */
+	Command(Statement, "drop table #odbctestdata");
+
+	Disconnect();
+
+	printf("Done.\n");
+	return 0;
+}
