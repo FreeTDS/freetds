b564ef42da19e31509da49ab52b1fd08c32e530a
James K. Lowden <jklowden@freetds.org>
2004-03-25 04:51:13 +0000

    more generalized processing, to allow more complex tests.  dbnumrets() fails if stored procedure returns multiple result sets.

--- src/dblib/unittests/rpc.c
+++ src/dblib/unittests/rpc.c
@@ -21,20 +21,31 @@
 
 #include "common.h"
 
-static char software_version[] = "$Id: rpc.c,v 1.10 2003-01-26 18:42:54 freddy77 Exp $";
+static char software_version[] = "$Id: rpc.c,v 1.11 2004-03-25 04:51:14 jklowden Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
-static char cmd[512];
+static char cmd[4096];
 static int init_proc(DBPROCESS * dbproc, const char *name);
+
+static const char procedure_sql[] = 
+		"CREATE PROCEDURE %s \n"
+			"@nrows int OUTPUT \n"
+		"AS \n"
+		"BEGIN \n"
+			"select distinct convert(varchar(30), name) as 'type'  from systypes \n"
+			"select @nrows = @@rowcount \n"
+			"select distinct convert(varchar(30), name) as name  from sysobjects where type = 'S'"
+			"return 42 \n"
+		"END \n";
 
 static int
 init_proc(DBPROCESS * dbproc, const char *name)
 {
 	int res = 0;
 
-	fprintf(stdout, "Dropping proc\n");
+	fprintf(stdout, "Dropping procedure %s\n", name);
 	add_bread_crumb();
-	sprintf(cmd, "drop proc %s", name);
+	sprintf(cmd, "DROP PROCEDURE %s", name);
 	dbcmd(dbproc, cmd);
 	add_bread_crumb();
 	dbsqlexec(dbproc);
@@ -44,16 +55,16 @@ init_proc(DBPROCESS * dbproc, const char *name)
 	}
 	add_bread_crumb();
 
-	fprintf(stdout, "creating proc\n");
-	sprintf(cmd, "create proc %s (@b int out) as\nbegin\n select @b = 42\nend\n", name);
+	fprintf(stdout, "Creating procedure %s\n", name);
+	sprintf(cmd, procedure_sql, name);
 	dbcmd(dbproc, cmd);
 	if (dbsqlexec(dbproc) == FAIL) {
 		add_bread_crumb();
 		res = 1;
 		if (name[0] == '#')
-			fprintf(stdout, "Failed to create proc %s. Wrong permission or not MSSQL.\n", name);
+			fprintf(stdout, "Failed to create procedure %s. Wrong permission or not MSSQL.\n", name);
 		else
-			fprintf(stdout, "Failed to create proc %s. Wrong permission.\n", name);
+			fprintf(stdout, "Failed to create procedure %s. Wrong permission.\n", name);
 	}
 	while (dbresults(dbproc) != NO_MORE_RESULTS) {
 		/* nop */
@@ -66,14 +77,14 @@ main(int argc, char **argv)
 {
 	LOGINREC *login;
 	DBPROCESS *dbproc;
-	int i;
+	int i, r;
 	char teststr[1024];
 	int failed = 0;
 	char *retname = NULL;
 	int rettype = 0, retlen = 0, retval = 0xdeadbeef;
-	char proc[] = "#t0022", param[] = "@b";
+	char proc[] = "#t0022", param[] = "@nrows";
 	char *proc_name = proc;
-	RETCODE erc;
+	RETCODE erc, row_code;
 
 	set_malloc_options();
 
@@ -143,30 +154,57 @@ main(int argc, char **argv)
 	add_bread_crumb();
 
 	/* retrieve outputs per usual */
-	printf("retrieving output parameter... ");
-	if (dbresults(dbproc) == FAIL) {
-		add_bread_crumb();
-		fprintf(stdout, "Was expecting a result set.\n");
-		exit(1);
-	}
-	printf("done\n");
-
-	add_bread_crumb();
-
-	for (i = 1; i <= dbnumrets(dbproc); i++) {
-		add_bread_crumb();
-		retname = dbretname(dbproc, i);
-		printf("ret name %d is %s\n", i, retname);
-		rettype = dbrettype(dbproc, i);
-		printf("ret type %d is %d\n", i, rettype);
-		retlen = dbretlen(dbproc, i);
-		printf("ret len %d is %d\n", i, retlen);
-		dbconvert(dbproc, rettype, dbretdata(dbproc, i), retlen, SYBVARCHAR, (BYTE*) teststr, -1);
-		printf("ret data %d is %s\n", i, teststr);
-		add_bread_crumb();
-	}
-	if ((retname == NULL) || strcmp(retname, "@b")) {
-		fprintf(stdout, "Was expecting a retname to be @b.\n");
+	r = 0;
+	while ((erc = dbresults(dbproc)) != NO_MORE_RESULTS) {
+		if (erc == SUCCEED) {
+			const int ncols = dbnumcols(dbproc);
+			printf("bound 1 of %d columns in result %d.\n", ncols, ++r);
+			dbbind(dbproc, 1, STRINGBIND, -1, (BYTE *) teststr);
+			
+			while ((row_code = dbnextrow(dbproc)) != NO_MORE_ROWS) {
+				if (row_code == REG_ROW) {
+					printf("\t%s\n", teststr);
+				} else {
+					/* not supporting computed rows in this unit test */
+					failed = 1;
+					fprintf(stderr, "Failed.  Expected a row\n");
+					exit(1);
+				}
+			}
+			/* check return status */
+			printf("retrieving return status... ");
+			if (dbhasretstat(dbproc) == TRUE) {
+				printf("(return status %d)\n", dbretstatus(dbproc));
+			} else {
+				printf("none\n");
+			}
+			/* check return parameter values */
+			printf("retrieving output parameter... ");
+			if (dbnumrets(dbproc) > 0) {
+				for (i = 1; i <= dbnumrets(dbproc); i++) {
+					add_bread_crumb();
+					retname = dbretname(dbproc, i);
+					printf("ret name %d is %s\n", i, retname);
+					rettype = dbrettype(dbproc, i);
+					printf("ret type %d is %d\n", i, rettype);
+					retlen = dbretlen(dbproc, i);
+					printf("ret len %d is %d\n", i, retlen);
+					dbconvert(dbproc, rettype, dbretdata(dbproc, i), retlen, SYBVARCHAR, (BYTE*) teststr, -1);
+					printf("ret data %d is %s\n", i, teststr);
+					add_bread_crumb();
+				}
+			} else {
+				printf("none\n");
+			}
+		} else {
+			add_bread_crumb();
+			fprintf(stdout, "Was expecting a result set.\n");
+			exit(1);
+		}
+	} /* while dbresults */
+	
+	if ((retname == NULL) || strcmp(retname, param)) {
+		fprintf(stdout, "Was expecting a retname to be '%s'.\n", param);
 		exit(1);
 	}
 	if (strcmp(teststr, "42")) {
@@ -182,9 +220,14 @@ main(int argc, char **argv)
 		exit(1);
 	}
 
-	fprintf(stdout, "Dropping proc\n");
+	printf("done\n");
+
 	add_bread_crumb();
-	sprintf(cmd, "drop proc %s", proc_name);
+
+
+	fprintf(stdout, "Dropping procedure\n");
+	add_bread_crumb();
+	sprintf(cmd, "DROP PROCEDURE %s", proc_name);
 	dbcmd(dbproc, cmd);
 	add_bread_crumb();
 	dbsqlexec(dbproc);
