54af9f317aabca5bdd026a0027661cac6acc90c0
Frediano Ziglio <freddy77@gmail.com>
2004-03-31 18:40:54 +0000

    fix for Sybase

--- src/odbc/unittests/error.c
+++ src/odbc/unittests/error.c
@@ -2,7 +2,7 @@
 
 /* some tests on error reporting */
 
-static char software_version[] = "$Id: error.c,v 1.1 2004-03-30 15:14:12 freddy77 Exp $";
+static char software_version[] = "$Id: error.c,v 1.2 2004-03-31 18:40:54 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
 static SQLCHAR output[256];
@@ -21,6 +21,8 @@ ReadError(void)
 int
 main(int argc, char *argv[])
 {
+	SQLRETURN retcode;
+
 	Connect();
 
 	/* create a test table */
@@ -32,17 +34,23 @@ main(int argc, char *argv[])
 	Command(Statement, "insert into #tmp values(7)");
 
 	/* issue our command */
-	if (CommandWithResult(Statement, "select 100 / (i - 5) from #tmp order by i") != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("Error in command");
+	retcode = CommandWithResult(Statement, "select 100 / (i - 5) from #tmp order by i");
 
-	/* TODO when multiple row fetch available test for error on some columns */
+	/* special case, Sybase detect error early */
+	if (retcode != SQL_ERROR || db_is_microsoft()) {
 
-	if (SQLFetch(Statement) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLFetch failed when it shouldn't");
-	if (SQLFetch(Statement) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLFetch failed when it shouldn't");
-	if (SQLFetch(Statement) != SQL_ERROR)
-		ODBC_REPORT_ERROR("SQLFetch succeed when it shouldn't");
+		if (retcode != SQL_SUCCESS)
+			ODBC_REPORT_ERROR("Error in command");
+
+		/* TODO when multiple row fetch available test for error on some columns */
+
+		if (SQLFetch(Statement) != SQL_SUCCESS)
+			ODBC_REPORT_ERROR("SQLFetch failed when it shouldn't");
+		if (SQLFetch(Statement) != SQL_SUCCESS)
+			ODBC_REPORT_ERROR("SQLFetch failed when it shouldn't");
+		if (SQLFetch(Statement) != SQL_ERROR)
+			ODBC_REPORT_ERROR("SQLFetch succeed when it shouldn't");
+	}
 
 	ReadError();
 	if (!strstr((char *) output, "zero")) {
