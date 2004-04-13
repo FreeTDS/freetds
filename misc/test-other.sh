ce38663494889fd2d607b60a1cc6ef5d22aebcf2
Frediano Ziglio <freddy77@gmail.com>
2004-04-13 08:49:21 +0000

    additional test

---
+++ misc/test-other.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Additional make check test
+# check other tools (like Perl and PHP)
+# odbc MUST be configured to point to source driver
+
+# stop on errors
+set -e
+
+# set correct directory
+DIR=`dirname $0`
+cd "$DIR/.."
+
+# save directory
+ORIGDIR="$PWD"
+
+# init log
+LOG="$PWD/test-other.log"
+rm -f "$LOG"
+echo "log started" >> "$LOG"
+
+## assure make
+#make all
+#echo "make all ok" >> "$LOG"
+
+tUID=`cat ../PWD | grep '^UID=' | sed 's,^....,,g'`
+tPWD=`cat ../PWD | grep '^PWD=' | sed 's,^....,,g'`
+tSRV=`cat ../PWD | grep '^SRV=' | sed 's,^....,,g'`
+
+# Perl
+if perl --help > /dev/null 2>&1; then
+	DBI_DSN="dbi:ODBC:$tSRV"
+	DBI_USER="$tUID"
+	DBI_PASS="$tPWD"
+	export DBI_DSN DBI_USER DBI_PASS
+	# TODO better way
+	ODBCHOME=/usr
+	export ODBCHOME
+	for f in DBD-ODBC-*.tar.gz; do
+		DIR=`echo "$f" | sed s,.tar.gz$,,g`
+		echo Testing $DIR
+		if ! test -d "$DIR"; then
+			tar zxvf "$DIR.tar.gz"
+		fi
+		test -d "$DIR"
+		cd "$DIR"
+		test -r Makefile || perl Makefile.PL
+		make
+		make test
+	done
+fi
+
+echo "all tests ok!!!" >> "$LOG"
+
