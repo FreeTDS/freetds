1f8fe03c164f62bb9988e3aede87a21f485a434c
Frediano Ziglio <freddy77@gmail.com>
2005-01-19 19:33:44 +0000

    add test for timeout

---
+++ src/odbc/unittests/timeout.c
@@ -0,0 +1,95 @@
+#include "common.h"
+#include <assert.h>
+
+/* Test timeout of query */
+
+static char software_version[] = "$Id: timeout.c,v 1.1 2005-01-19 19:33:49 freddy77 Exp $";
+static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
+
+static void
+AutoCommit(void)
+{
+	SQLRETURN ret;
+
+	ret = SQLSetConnectAttr(Connection, SQL_ATTR_AUTOCOMMIT, (void *) SQL_AUTOCOMMIT_OFF, 0);
+	if (ret != SQL_SUCCESS)
+		ODBC_REPORT_ERROR("Enabling AutoCommit");
+}
+
+static void
+EndTransaction(SQLSMALLINT type)
+{
+	SQLRETURN ret;
+
+	ret = SQLEndTran(SQL_HANDLE_DBC, Connection, type);
+	if (ret != SQL_SUCCESS)
+		ODBC_REPORT_ERROR("Can't commit transaction");
+}
+
+int
+main(int argc, char *argv[])
+{
+	HENV env;
+	HDBC dbc;
+	HSTMT stmt;
+	SQLRETURN ret;
+	SQLINTEGER i;
+
+	Connect();
+
+	CommandWithResult(Statement, "drop table test_timeout");
+	Command(Statement, "create table test_timeout(n numeric(18,0) primary key, t varchar(30))");
+	AutoCommit();
+
+	Command(Statement, "insert into test_timeout(n, t) values(1, 'initial')");
+	EndTransaction(SQL_COMMIT);
+
+	Command(Statement, "update test_timeout set t = 'second' where n = 1");
+
+	/* save this connection and do another */
+	env = Environment;
+	dbc = Connection;
+	stmt = Statement;
+	Environment = SQL_NULL_HENV;
+	Connection = SQL_NULL_HDBC;
+	Statement = SQL_NULL_HSTMT;
+
+	Connect();
+
+	AutoCommit();
+	ret = SQLSetStmtAttr(Statement, SQL_ATTR_QUERY_TIMEOUT, (SQLPOINTER) 2, 0);
+	if (ret != SQL_SUCCESS)
+		ODBC_REPORT_ERROR("Error setting timeout");
+
+	i = 1;
+	ret = SQLBindParameter(Statement, 1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_INTEGER, 0, 0, &i, 0, NULL);
+	if (ret != SQL_SUCCESS)
+		ODBC_REPORT_ERROR("SQLBindParameter failure");
+
+	if (SQLPrepare(Statement, "update test_timeout set t = 'bad' where n = ?", SQL_NTS) != SQL_SUCCESS)
+		ODBC_REPORT_ERROR("SQLPrepare failure");
+	ret = SQLExecute(Statement);
+	if (ret != SQL_ERROR)
+		ODBC_REPORT_ERROR("SQLExecute success ??");
+	EndTransaction(SQL_ROLLBACK);
+
+	/* TODO should return error S1T00 Timeout expired */
+	ret = CommandWithResult(Statement, "update test_timeout set t = 'bad' where n = 1");
+	if (ret != SQL_ERROR)
+		ODBC_REPORT_ERROR("SQLExecDirect success ??");
+	EndTransaction(SQL_ROLLBACK);
+
+	Disconnect();
+
+	Environment = env;
+	Connection = dbc;
+	Statement = stmt;
+
+	EndTransaction(SQL_COMMIT);
+	Command(Statement, "drop table test_timeout");
+	EndTransaction(SQL_COMMIT);
+
+	Disconnect();
+
+	return 0;
+}
