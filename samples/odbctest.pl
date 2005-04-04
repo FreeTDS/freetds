41c8636e9d63c4e2b24568aa2665bd9ee032fe47
James K. Lowden <jklowden@freetds.org>
2005-04-04 22:05:06 +0000

    made a little more useful

--- samples/odbctest.pl
+++ samples/odbctest.pl
@@ -1,16 +1,24 @@
 #!/usr/local/bin/perl
-#
+# $Id: odbctest.pl,v 1.2 2005-04-04 22:05:33 jklowden Exp $
+
 use DBI;
 
-my $dbh = DBI->connect("dbi:ODBC:JDBC", 'guest', 'sybase', {PrintError => 0});
+my ($dsn, $user, $pass, $sql) = @ARGV;
+
+$dsn  = "dbi:ODBC:JDBC" unless $dsn;
+$user = 'guest'  unless $user;
+$pass = 'sybase' unless $pass;
+
+$sql ="select \@\@servername" unless $sql;
+
+my $dbh = DBI->connect($dsn, $user, $pass, {PrintError => 1});
 
 die "Unable for connect to server $DBI::errstr"
     unless $dbh;
 
 my $rc;
-my $sth;
 
-$sth = $dbh->prepare("select \@\@servername");
+my $sth = $dbh->prepare($sql);
 if($sth->execute) {
     while(@dat = $sth->fetchrow) {
 		print "@dat\n";
