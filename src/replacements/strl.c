fc015c8a3787b3461263308c86f85c89a8f8e8a7
Frediano Ziglio <freddy77@gmail.com>
2005-07-15 12:07:05 +0000

    fix bad copy-paste

--- src/replacements/strl.c
+++ src/replacements/strl.c
@@ -1,5 +1,5 @@
 /* FreeTDS - Library of routines accessing Sybase and Microsoft databases
- * Copyright (C) 2003, 2004  James K. Lowden, based on original work by Brian Bruns
+ * Copyright (C) 2005 Ziglio Frediano
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -17,19 +17,6 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/*
- * This file implements a very simple iconv.
- * Its purpose is to allow ASCII clients to communicate with Microsoft servers
- * that encode their metadata in Unicode (UCS-2).
- *
- * The conversion algorithm relies on the fact that UCS-2 shares codepoints
- * between 0 and 255 with ISO-8859-1.  To create UCS-2, we add a high byte
- * whose value is zero.  To create ISO-8859-1, we strip the high byte.
- *
- * If we receive an input character whose value is greater than 255, we return an
- * out-of-range error.  The caller (tds_iconv) should emit an error message.
- */
-
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -41,7 +28,7 @@
 #include "tds_sysdep_private.h"
 #include "replacements.h"
 
-TDS_RCSID(var, "$Id: strl.c,v 1.2 2005-07-15 11:52:18 freddy77 Exp $");
+TDS_RCSID(var, "$Id: strl.c,v 1.3 2005-07-15 12:07:05 freddy77 Exp $");
 
 #if ! HAVE_STRLCPY
 size_t 
