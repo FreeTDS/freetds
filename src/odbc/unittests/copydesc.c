c2189e31e31d61c0eab41b27d86e7c37e2329443
Frediano Ziglio <freddy77@gmail.com>
2006-05-13 08:48:49 +0000

    improve copydesc test

--- src/odbc/unittests/copydesc.c
+++ src/odbc/unittests/copydesc.c
@@ -2,18 +2,24 @@
 
 /* Test SQLCopyDesc */
 
-static char software_version[] = "$Id: copydesc.c,v 1.1 2006-05-08 09:39:08 freddy77 Exp $";
+static char software_version[] = "$Id: copydesc.c,v 1.2 2006-05-13 08:48:49 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
 int
 main(int argc, char *argv[])
 {
 	SQLHDESC ard, ard2;
+	SQLINTEGER id;
+	SQLLEN ind1, ind2;
+	char name[64];
 
 	Connect();
 
 	if (SQLGetStmtAttr(Statement, SQL_ATTR_APP_ROW_DESC, &ard, 0, NULL) != SQL_SUCCESS)
 		ODBC_REPORT_ERROR("SQLGetStmtAttr");
+
+	SQLBindCol(Statement, 1, SQL_C_SLONG, &id, sizeof(SQLINTEGER), &ind1);
+	SQLBindCol(Statement, 2, SQL_C_CHAR, name, sizeof(name), &ind2);
 
 	if (SQLAllocHandle(SQL_HANDLE_DESC, Connection, &ard2) != SQL_SUCCESS)
 		ODBC_REPORT_ERROR("SQLAllocHandle");
