b483df43d48d109536f9fa9e3d45f5bbab9ab53a
Frediano Ziglio <freddy77@gmail.com>
2006-06-13 12:38:29 +0000

    added a test for connection close

---
+++ src/odbc/unittests/prepclose.c
@@ -0,0 +1,103 @@
+#include "common.h"
+
+#if HAVE_UNISTD_H
+#include <unistd.h>
+#endif /* HAVE_UNISTD_H */
+
+#if HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif /* HAVE_SYS_SOCKET_H */
+
+#if HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif /* HAVE_SYS_STAT_H */
+
+#if HAVE_SYS_IOCTL_H
+#include <sys/ioctl.h>
+#endif /* HAVE_SYS_IOCTL_H */
+
+#if HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif /* HAVE_SYS_WAIT_H */
+
+/* test error on connection close */
+
+static char software_version[] = "$Id: prepclose.c,v 1.1 2006-06-13 12:38:29 freddy77 Exp $";
+static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
+
+#if HAVE_FSTAT && defined(S_IFSOCK)
+
+static int
+close_last_socket(void)
+{
+	int max_socket = -1, i;
+	int sockets[2];
+
+	for (i = 3; i < 1024; ++i) {
+		struct stat file_stat;
+		if (fstat(i, &file_stat))
+			continue;
+		if ((file_stat.st_mode & S_IFSOCK) == S_IFSOCK)
+			max_socket = i;
+	}
+	if (max_socket < 0)
+		return 0;
+
+	/* replace socket with a new one */
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) < 0)
+		return 0;
+
+	/* substitute socket */
+	close(max_socket);
+	dup2(sockets[0], max_socket);
+
+	/* close connection */
+	close(sockets[0]);
+	close(sockets[1]);
+	return 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+	char buf[256];
+	SQLRETURN ret;
+	unsigned char sqlstate[6];
+
+	Connect();
+
+	if (!close_last_socket()) {
+		fprintf(stderr, "Error closing connection\n");
+		return 1;
+	}
+
+	/* force disconnection closing socket */
+	ret = SQLPrepare(Statement, (SQLCHAR *) "SELECT 1", SQL_NTS);
+	if (ret != SQL_ERROR) {
+		fprintf(stderr, "Error expected\n");
+		return 1;
+	}
+
+	ret = SQLGetDiagRec(SQL_HANDLE_STMT, Statement, 1, sqlstate, NULL, (SQLCHAR *) buf, sizeof(buf), NULL);
+	if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO) {
+		fprintf(stderr, "Error not set (line %d)\n", __LINE__);
+		return 1;
+	}
+	printf("err=%s\n", buf);
+	
+	Disconnect();
+
+	printf("Done.\n");
+	return 0;
+}
+
+#else
+#error mao
+int
+main(int argc, char *argv[])
+{
+	printf("Not possible for this platform.\n");
+	return 0;
+}
+#endif
+
