e0c0c48f96f5b11d58abe6942ebae919db98f26e
Frediano Ziglio <freddy77@gmail.com>
2006-06-21 15:04:02 +0000

    rewrote a test

--- src/odbc/unittests/describecol.c
+++ src/odbc/unittests/describecol.c
@@ -1,166 +1,331 @@
 #include "common.h"
+#include <stdarg.h>
+#include <ctype.h>
 
 /*
  * SQLDescribeCol test for precision
  * test what say SQLDescribeCol about precision using some type
  */
 
-static char software_version[] = "$Id: describecol.c,v 1.6 2006-06-20 12:33:59 freddy77 Exp $";
+static char software_version[] = "$Id: describecol.c,v 1.7 2006-06-21 15:04:02 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
 static int g_result = 0;
-static const char *curr_test = NULL;
+static unsigned int line_num;
 
 static void
-check_attr(const char *what, int got)
+fatal(const char *msg, ...)
 {
-	int n;
-	char *p = strtok(NULL, " \t");
+	va_list ap;
 
-	if (!p) {
-		fprintf(stderr, "Int not found parsing test '%s'\n", curr_test);
-		exit(1);
-	}
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	va_end(ap);
 
-	n = atoi(p);
-
-	if (got != n) {
-		g_result = 1;
-		fprintf(stderr, "Expected SQLColAttribute(%s) %d got %d\n", what, n, got);
-	}
+	exit(1);
 }
 
-static void
-DoTest(const char *test_in)
+static int
+get_int(const char *s)
 {
-	SQLCHAR output[16];
-	char sql[256];
-	char test_buf[256];
-	char *type, *val;
+	char *end;
+	long l;
 
-	SQLSMALLINT colType;
-	SQLULEN colSize;
-	SQLSMALLINT colScale, colNullable;
-	SQLINTEGER iColLen, iColPrec, iColScale;
-	SQLINTEGER iDescLen, iDescPrec, iScale, iOctetLen, iDisplaySize;
-
-	curr_test = test_in;
-
-	strcpy(test_buf, test_in);
-	type = strtok(test_buf, " \t");
-	val = strtok(NULL, " \t");
-
-	sprintf(sql, "SELECT CONVERT(%s, %s) AS col", type, val);
-	/* ignore error, we only need precision of known types */
-	if (CommandWithResult(Statement, sql) != SQL_SUCCESS) {
-		ResetStatement();
-		return;
-	}
-
-	if (!SQL_SUCCEEDED(SQLFetch(Statement)))
-		ODBC_REPORT_ERROR("Unable to fetch row");
-
-	colSize = 0xbeef;
-	if (SQLDescribeCol(Statement, 1, output, sizeof(output), NULL, &colType, &colSize, &colScale, &colNullable) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("Error getting data");
-
-	iColLen = iColPrec = iColScale = 0xdeadbeef;
-	if (SQLColAttribute(Statement, 1, SQL_COLUMN_LENGTH, NULL, SQL_IS_INTEGER, NULL, &iColLen) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-	if (SQLColAttribute(Statement, 1, SQL_COLUMN_PRECISION, NULL, SQL_IS_INTEGER, NULL, &iColPrec) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-	if (SQLColAttribute(Statement, 1, SQL_COLUMN_SCALE, NULL, SQL_IS_INTEGER, NULL, &iColScale) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-
-	iDescLen = iDescPrec = iScale = iOctetLen = iDisplaySize = 0xdeadbeef;
-	if (SQLColAttribute(Statement, 1, SQL_DESC_LENGTH, NULL, SQL_IS_INTEGER, NULL, &iDescLen) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-	if (SQLColAttribute(Statement, 1, SQL_DESC_OCTET_LENGTH, NULL, SQL_IS_INTEGER, NULL, &iOctetLen) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-	if (SQLColAttribute(Statement, 1, SQL_DESC_PRECISION, NULL, SQL_IS_INTEGER, NULL, &iDescPrec) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-	if (SQLColAttribute(Statement, 1, SQL_DESC_SCALE, NULL, SQL_IS_INTEGER, NULL, &iScale) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-	if (SQLColAttribute(Statement, 1, SQL_DESC_DISPLAY_SIZE, NULL, SQL_IS_INTEGER, NULL, &iDisplaySize) != SQL_SUCCESS)
-		ODBC_REPORT_ERROR("SQLColAttribute");
-
-	printf(" %s prec %d\n"
-	       "\tCOLUMN_LENGTH %d COLUMN_PRECISION %d COLUMN_SCALE %d\n"
-	       "\tDESC_LENGTH %d DESC_OCTET_LENGTH %d DESC_PRECISION %d DESC_SCALE %d\n",
-	       type, (int) colSize,
-	       (int) iColLen, (int) iColPrec, (int) iColScale,
-	       (int) iDescLen, (int) iOctetLen, (int) iDescPrec, (int) iScale);
-
-	if (colSize != iDescLen) {
-		g_result = 1;
-		fprintf(stderr, "SQLDescribeCol(PRECISION) not the same as SQLColAttribute(SQL_DESC_LENGTH)\n");
-	}
-
-	check_attr("COLUMN_LENGTH", iColLen);
-	check_attr("COLUMN_PRECISION", iColPrec);
-	check_attr("COLUMN_SCALE", iColScale);
-	check_attr("DESC_LENGTH", iDescLen);
-	check_attr("DESC_OCTET_LENGTH", iOctetLen);
-	check_attr("DESC_PRECISION", iDescPrec);
-	check_attr("DESC_SCALE", iScale);
-	check_attr("DESC_DISPLAY_SIZE", iDisplaySize);
-
-	SQLMoreResults(Statement);
-	ResetStatement();
+	if (!s)
+		fatal("Line %u: NULL int\n", line_num);
+	l = strtol(s, &end, 0);
+	if (end[0])
+		fatal("Line %u: Invalid int\n", line_num);
+	return (int) l;
 }
 
-static void
-AllTypes(void)
+struct lookup_int
 {
-	Connect();
+	const char *name;
+	int value;
+};
 
-	Command(Statement, "SET TEXTSIZE 4096");
+static int
+lookup(const char *name, const struct lookup_int *table)
+{
+	if (!table)
+		return get_int(name);
 
-	DoTest("bit      0  1  1 0  1 1  1 0  1");
-	DoTest("tinyint  0  1  3 0  3 1  3 0  3");
-	DoTest("smallint 0  2  5 0  5 2  5 0  6");
-	DoTest("int      0  4 10 0 10 4 10 0 11");
-	DoTest("bigint   0  8 19 0 19 8 19 0 20");
+	for (; table->name; ++table)
+		if (strcmp(table->name, name) == 0)
+			return table->value;
 
-	if (use_odbc_version3) {
-		DoTest("real  0 4  7 0 24 4 24 0 14");
-		DoTest("float 0 8 15 0 53 8 53 0 24");
-	} else {
-		DoTest("real  0 4  7 0  7 4  7 0 14");
-		DoTest("float 0 8 15 0 15 8 15 0 24");
+	return get_int(name);
+}
+
+static struct lookup_int sql_types[] = {
+#define TYPE(s) { #s, s }
+	TYPE(SQL_CHAR),
+	TYPE(SQL_VARCHAR),
+	TYPE(SQL_LONGVARCHAR),
+	TYPE(SQL_WCHAR),
+	TYPE(SQL_WVARCHAR),
+	TYPE(SQL_WLONGVARCHAR),
+	TYPE(SQL_DECIMAL),
+	TYPE(SQL_NUMERIC),
+	TYPE(SQL_SMALLINT),
+	TYPE(SQL_INTEGER),
+	TYPE(SQL_REAL),
+	TYPE(SQL_FLOAT),
+	TYPE(SQL_DOUBLE),
+	TYPE(SQL_BIT),
+	TYPE(SQL_TINYINT),
+	TYPE(SQL_BIGINT),
+	TYPE(SQL_BINARY),
+	TYPE(SQL_VARBINARY),
+	TYPE(SQL_LONGVARBINARY),
+	TYPE(SQL_TYPE_DATE),
+	TYPE(SQL_TYPE_TIME),
+	TYPE(SQL_TYPE_TIMESTAMP)
+#undef TYPE
+};
+
+typedef enum
+{
+	type_INTEGER,
+	type_SMALLINT,
+	type_LEN,
+	type_CHARP
+} test_type_t;
+
+struct attribute
+{
+	const char *name;
+	int value;
+	test_type_t type;
+	const struct lookup_int *lookup;
+};
+
+static const struct attribute attributes[] = {
+#define ATTR(s,t) { #s, s, type_##t, NULL }
+#define ATTR2(s,t,l) { #s, s, type_##t, l }
+	ATTR(SQL_COLUMN_LENGTH, INTEGER),
+	ATTR(SQL_COLUMN_PRECISION, INTEGER),
+	ATTR(SQL_COLUMN_SCALE, INTEGER),
+	ATTR(SQL_DESC_LENGTH, LEN),
+	ATTR(SQL_DESC_OCTET_LENGTH, LEN),
+	ATTR(SQL_DESC_PRECISION, SMALLINT),
+	ATTR(SQL_DESC_SCALE, SMALLINT),
+	ATTR(SQL_DESC_DISPLAY_SIZE, INTEGER),
+	ATTR2(SQL_DESC_CONCISE_TYPE, SMALLINT, sql_types),
+	ATTR2(SQL_DESC_TYPE, SMALLINT, sql_types)
+#undef ATTR2
+#undef ATTR
+};
+
+static const struct attribute *
+lookup_attr(const char *name)
+{
+	int i;
+
+	if (!name)
+		fatal("Line %u: NULL attribute\n", line_num);
+	for (i = 0; i < sizeof(attributes) / sizeof(attributes[0]); ++i)
+		if (strcmp(attributes[i].name, name) == 0 || strcmp(attributes[i].name + 4, name) == 0)
+			return &attributes[i];
+	fatal("Line %u: attribute %s not found\n", line_num, name);
+	return NULL;
+}
+
+#define SEP " \t\n"
+
+#define ATTR_PARAMS const struct attribute *attr, int expected
+typedef int (*get_attr_t) (ATTR_PARAMS);
+
+static int
+get_attr_ird(ATTR_PARAMS)
+{
+	SQLINTEGER i;
+	SQLRETURN ret;
+
+	if (attr->type == type_CHARP)
+		fatal("Line %u: CHAR* check still not supported\n", line_num);
+	i = 0xdeadbeef;
+	ret = SQLColAttribute(Statement, 1, attr->value, NULL, SQL_IS_INTEGER, NULL, &i);
+	if (!SQL_SUCCEEDED(ret))
+		fatal("Line %u: failure not expected\n", line_num);
+	return i;
+}
+
+static int
+get_attr_ard(ATTR_PARAMS)
+{
+	SQLINTEGER i, ind;
+	SQLSMALLINT si;
+	SQLLEN li;
+	SQLRETURN ret;
+	SQLHDESC desc = SQL_NULL_DESC;
+
+	/* get ARD */
+	SQLGetStmtAttr(Statement, SQL_ATTR_APP_ROW_DESC, &desc, sizeof(desc), &ind);
+
+	ret = SQL_ERROR;
+	switch (attr->type) {
+	case type_INTEGER:
+		i = 0xdeadbeef;
+		ret = SQLGetDescField(desc, 1, attr->value, (SQLPOINTER) & i, sizeof(SQLINTEGER), &ind);
+		break;
+	case type_SMALLINT:
+		si = 0xbeef;
+		ret = SQLGetDescField(desc, 1, attr->value, (SQLPOINTER) & si, sizeof(SQLSMALLINT), &ind);
+		i = si;
+		break;
+	case type_LEN:
+		li = 0xdeadbeef;
+		ret = SQLGetDescField(desc, 1, attr->value, (SQLPOINTER) & li, sizeof(SQLLEN), &ind);
+		i = li;
+		break;
+	case type_CHARP:
+		fatal("Line %u: CHAR* check still not supported\n", line_num);
+		break;
 	}
+	if (!SQL_SUCCEEDED(ret))
+		fatal("Line %u: failure not expected\n", line_num);
+	return i;
+}
 
-	DoTest("smallmoney 0 12 10 4 10 12 10 4 12");
-	DoTest("money      0 21 19 4 19 21 19 4 21");
-
-	DoTest("numeric(10,2) 0 12 10 2 10 12 10 2 12");
-	DoTest("numeric(23,4) 0 25 23 4 23 25 23 4 25");
-
-	DoTest("datetime      '2006-04-14' 16 23 3 23 16 3 3 23");
-	DoTest("smalldatetime '2006-04-14' 16 16 0 16 16 0 0 19");
-
-	DoTest("char(10)     'hi!'   10   10 0   10   10   10 0   10");
-	DoTest("varchar(11)  'hi!'   11   11 0   11   11   11 0   11");
-	DoTest("nchar(12)    'hi!'   24   12 0   12   24   12 0   12");
-	DoTest("nvarchar(13) 'hi!'   26   13 0   13   26   13 0   13");
-	DoTest("text         'hi!' 4096 4096 0 4096 4096 4096 0 4096");
-	DoTest("ntext        'hi!' 4096 2048 0 2048 4096 2048 0 2048");
-
-	DoTest("binary(10)    'hi!'   10   10 0   10   10   10 0   20");
-	DoTest("varbinary(11) 'hi!'   11   11 0   11   11   11 0   22");
-	DoTest("image         'hi!' 4096 4096 0 4096 4096 4096 0 8192");
-
-	Disconnect();
+/* do not retry any attribute just return expected value so to make caller happy */
+static int
+get_attr_none(ATTR_PARAMS)
+{
+	return expected;
 }
 
 int
 main(int argc, char *argv[])
 {
-	use_odbc_version3 = 0;
-	AllTypes();
+#define TEST_FILE "describecol.in"
+	const char *in_file = FREETDS_SRCDIR "/" TEST_FILE;
+	FILE *f;
+	char buf[256];
+	SQLINTEGER i, len;
+	get_attr_t get_attr_p = get_attr_none;
 
-	use_odbc_version3 = 1;
-	AllTypes();
+	Connect();
+	Command(Statement, "SET TEXTSIZE 4096");
+
+	SQLBindCol(Statement, 1, SQL_C_SLONG, &i, sizeof(i), &len);
+
+	f = fopen(in_file, "r");
+	if (!f)
+		fopen(TEST_FILE, "r");
+	if (!f) {
+		fprintf(stderr, "error opening test file\n");
+		exit(1);
+	}
+
+	line_num = 0;
+	while (fgets(buf, sizeof(buf), f)) {
+		char *p = buf, *cmd;
+
+		++line_num;
+
+		while (isspace((unsigned char) *p))
+			++p;
+		cmd = strtok(p, SEP);
+
+		/* skip comments */
+		if (!cmd || cmd[0] == '#' || cmd[0] == 0 || cmd[0] == '\n')
+			continue;
+
+		if (strcmp(cmd, "odbc") == 0) {
+			int odbc3 = get_int(strtok(NULL, SEP)) == 3 ? 1 : 0;
+
+			if (use_odbc_version3 != odbc3) {
+				use_odbc_version3 = odbc3;
+				Disconnect();
+				Connect();
+				Command(Statement, "SET TEXTSIZE 4096");
+				SQLBindCol(Statement, 1, SQL_C_SLONG, &i, sizeof(i), &len);
+			}
+		}
+
+		/* select type */
+		if (strcmp(cmd, "select") == 0) {
+			const char *type = strtok(NULL, SEP);
+			const char *value = strtok(NULL, SEP);
+			char sql[sizeof(buf) + 40];
+
+			SQLMoreResults(Statement);
+			ResetStatement();
+
+			sprintf(sql, "SELECT CONVERT(%s, %s) AS col", type, value);
+
+			/* ignore error, we only need precision of known types */
+			get_attr_p = get_attr_none;
+			if (CommandWithResult(Statement, sql) != SQL_SUCCESS) {
+				ResetStatement();
+				SQLBindCol(Statement, 1, SQL_C_SLONG, &i, sizeof(i), &len);
+				continue;
+			}
+
+			if (!SQL_SUCCEEDED(SQLFetch(Statement)))
+				ODBC_REPORT_ERROR("Unable to fetch row");
+			SQLBindCol(Statement, 1, SQL_C_SLONG, &i, sizeof(i), &len);
+			get_attr_p = get_attr_ird;
+		}
+
+		/* set attribute */
+		if (strcmp(cmd, "set") == 0) {
+			const struct attribute *attr = lookup_attr(strtok(NULL, SEP));
+			char *value = strtok(NULL, SEP);
+			SQLHDESC desc;
+			SQLRETURN ret;
+			SQLINTEGER ind;
+
+			if (!value)
+				fatal("Line %u: value not defined\n", line_num);
+
+			/* get ARD */
+			SQLGetStmtAttr(Statement, SQL_ATTR_APP_ROW_DESC, &desc, sizeof(desc), &ind);
+
+			ret = SQL_ERROR;
+			switch (attr->type) {
+			case type_INTEGER:
+				ret = SQLSetDescField(desc, 1, attr->value, (SQLPOINTER) lookup(value, attr->lookup),
+						      sizeof(SQLINTEGER));
+				break;
+			case type_SMALLINT:
+				ret = SQLSetDescField(desc, 1, attr->value, (SQLPOINTER) lookup(value, attr->lookup),
+						      sizeof(SQLSMALLINT));
+				break;
+			case type_LEN:
+				ret = SQLSetDescField(desc, 1, attr->value, (SQLPOINTER) lookup(value, attr->lookup),
+						      sizeof(SQLLEN));
+				break;
+			case type_CHARP:
+				ret = SQLSetDescField(desc, 1, attr->value, (SQLPOINTER) value, SQL_NTS);
+				break;
+			}
+			if (!SQL_SUCCEEDED(ret))
+				fatal("Line %u: failure not expected setting ARD attribute\n", line_num);
+			get_attr_p = get_attr_ard;
+		}
+
+		/* test attribute */
+		if (strcmp(cmd, "attr") == 0) {
+			const struct attribute *attr = lookup_attr(strtok(NULL, SEP));
+			char *value = strtok(NULL, SEP);
+			int i, expected = lookup(value, attr->lookup);
+
+			if (!value)
+				fatal("Line %u: value not defined\n", line_num);
+
+			i = get_attr_p(attr, expected);
+			if (i != expected) {
+				g_result = 1;
+				fprintf(stderr, "Line %u: invalid %s got %d expected %d\n", line_num, attr->name, i, expected);
+			}
+		}
+	}
+
+	fclose(f);
+	Disconnect();
 
 	printf("Done.\n");
 	return g_result;
