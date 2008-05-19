bb14b9566061acb3467c3fd039d114fdef154879
James K. Lowden <jklowden@freetds.org>
2008-05-19 14:23:52 +0000

    applied ML patch today for BLK_VERSION for sqsh

--- include/cspublic.h
+++ include/cspublic.h
@@ -34,7 +34,7 @@ extern "C"
 #define TDS_STATIC_CAST(type, a) ((type)(a))
 #endif
 
-static const char rcsid_cspublic_h[] = "$Id: cspublic.h,v 1.58 2007-06-25 09:48:20 freddy77 Exp $";
+static const char rcsid_cspublic_h[] = "$Id: cspublic.h,v 1.58.2.1 2008-05-19 14:23:52 jklowden Exp $";
 static const void *const no_unused_cspublic_h_warn[] = { rcsid_cspublic_h, no_unused_cspublic_h_warn };
 
 #define CS_PUBLIC
@@ -447,6 +447,9 @@ enum
 
 #define BLK_VERSION_100 CS_VERSION_100
 #define BLK_VERSION_110 CS_VERSION_100
+#define BLK_VERSION_120 CS_VERSION_120
+#define BLK_VERSION_125 CS_VERSION_125
+#define BLK_VERSION_150 CS_VERSION_150
 
 #define CS_FORCE_EXIT	300
 #define CS_FORCE_CLOSE  301
