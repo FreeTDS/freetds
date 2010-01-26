38be043cecda221a42c5d61fb207b83ef02ded27
Frediano Ziglio <freddy77@gmail.com>
2010-01-26 11:10:51 +0000

    allow driver registration like ole controls

--- win32/winsetup.c
+++ win32/winsetup.c
@@ -50,6 +50,7 @@
 #include <assert.h>
 #include <ctype.h>
 #include <assert.h>
+#include <olectl.h>
 
 #include "resource.h"
 
@@ -57,6 +58,7 @@
 #include "tdsodbc.h"
 #include "tdsstring.h"
 #include "tdsconvert.h"
+#include "replacements.h"
 
 #ifdef DMALLOC
 #include <dmalloc.h>
@@ -415,3 +417,49 @@ ConfigTranslator(HWND hwndParent, DWORD * pvOption)
 {
 	return TRUE;
 }
+
+HRESULT WINAPI
+DllRegisterServer(void)
+{
+	TCHAR fn[MAX_PATH], full_fn[MAX_PATH];
+	LPTSTR name;
+	WORD len_out;
+	DWORD cnt;
+	char *desc = NULL;
+	BOOL b_res;
+
+	if (!GetModuleFileName(hinstFreeTDS, fn, TDS_VECTOR_SIZE(fn)))
+		return SELFREG_E_CLASS;
+	if (!GetFullPathName(fn, TDS_VECTOR_SIZE(full_fn), full_fn, &name) || !name || full_fn == name)
+		return SELFREG_E_CLASS;
+
+	if (asprintf(&desc, "FreeTDS%c"
+		"APILevel=2%c"
+		"ConnectFunctions=YYN%c"
+		"DriverODBCVer=03.00%c"
+		"FileUsage=0%c"
+		"SQLLevel=2%c"
+		"Setup=%s%c"
+		"Driver=%s%c",
+		0, 0, 0, 0, 0, 0,
+		name, 0, name, 0
+		) < 0)
+		return SELFREG_E_CLASS;
+	name[-1] = 0;
+
+	b_res = SQLInstallDriverEx(desc, full_fn, fn, TDS_VECTOR_SIZE(fn), &len_out, ODBC_INSTALL_COMPLETE, &cnt);
+	free(desc);
+	if (!b_res)
+		return SELFREG_E_CLASS;
+	return S_OK;
+}
+
+HRESULT WINAPI
+DllUnregisterServer(void)
+{
+	DWORD cnt;
+	if (!SQLRemoveDriver("FreeTDS", FALSE, &cnt))
+		return SELFREG_E_CLASS;
+	return S_OK;
+}
+
