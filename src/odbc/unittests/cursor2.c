392cd811e24a6dc366ada672cf3bf562a758a99d
Frediano Ziglio <freddy77@gmail.com>
2010-06-26 07:23:40 +0000

    add test for cursors on rpc

--- src/odbc/unittests/cursor2.c
+++ src/odbc/unittests/cursor2.c
@@ -1,8 +1,11 @@
 #include "common.h"
 
-/* Test cursor do not give error for statement that do not return rows  */
+/*
+ * 1) Test cursor do not give error for statement that do not return rows
+ * 2) Test cursor returns results on language RPCs
+ */
 
-static char software_version[] = "$Id: cursor2.c,v 1.7 2010-06-26 07:19:54 freddy77 Exp $";
+static char software_version[] = "$Id: cursor2.c,v 1.8 2010-06-26 07:23:40 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
 int
@@ -25,6 +28,21 @@ main(int argc, char *argv[])
 	Command("DROP TABLE #cursor2_test");
 
 	CHKGetDiagRec(SQL_HANDLE_STMT, Statement, 1, sqlstate, NULL, msg, sizeof(msg), NULL, "No");
+
+
+	ResetStatement();
+	CommandWithResult(Statement, "drop proc sp_test");
+	Command("create proc sp_test @name varchar(30) as select 0 as pippo select 1 as 'test', @name as 'nome'");
+
+	ResetStatement();
+	CHKSetStmtAttr(SQL_ATTR_CURSOR_TYPE, (SQLPOINTER) SQL_CURSOR_STATIC, SQL_IS_INTEGER, "S");
+
+	Command(" exec sp_test 'ciao'");
+
+	CHKFetch("S");
+
+	ResetStatement();
+	Command("drop proc sp_test");
 
 	Disconnect();
 	
