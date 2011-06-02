6f71462810b67f3dcaa42461cab66e8d724584f1
Frediano Ziglio <freddy77@gmail.com>
2011-06-02 19:21:05 +0000

    fix bcp conversion

--- src/dblib/bcp.c
+++ src/dblib/bcp.c
@@ -62,7 +62,7 @@
 #define MAX(a,b) ( (a) > (b) ? (a) : (b) )
 #endif
 
-TDS_RCSID(var, "$Id: bcp.c,v 1.197.2.5 2011-06-02 19:19:30 freddy77 Exp $");
+TDS_RCSID(var, "$Id: bcp.c,v 1.197.2.6 2011-06-02 19:21:05 freddy77 Exp $");
 
 #ifdef HAVE_FSEEKO
 typedef off_t offset_type;
@@ -1250,8 +1250,9 @@ _bcp_read_hostfile(DBPROCESS * dbproc, FILE * hostfile, int *row_error)
 			 */
 			file_len = collen;
 			if (bcpcol->char_conv) {
-				if (bcpcol->on_server.column_size > bcpcol->column_size)
-					collen = (collen * bcpcol->on_server.column_size) / bcpcol->column_size;
+				collen *= char_conv->server_charset.max_bytes_per_char;
+				collen += char_conv->client_charset.min_bytes_per_char - 1;
+				collen /= char_conv->client_charset.min_bytes_per_char;
 				cd = bcpcol->char_conv->to_wire;
 				tdsdump_log(TDS_DBG_FUNC, "Adjusted collen is %d.\n", collen);
 
