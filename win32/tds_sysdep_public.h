aae5077f27104c4323e77e93c8be1067389b7255
Frediano Ziglio <freddy77@gmail.com>
2011-08-04 04:32:09 +0000

    change header inclusion order to avoid wrong redeclarations

--- win32/tds_sysdep_public.h
+++ win32/tds_sysdep_public.h
@@ -20,7 +20,7 @@
 #ifndef _tds_sysdep_public_h_
 #define _tds_sysdep_public_h_
 
-static char rcsid_tds_sysdep_public_h[] = "$Id: tds_sysdep_public.h,v 1.8 2009-03-16 20:46:20 freddy77 Exp $";
+static char rcsid_tds_sysdep_public_h[] = "$Id: tds_sysdep_public.h,v 1.9 2011-08-04 04:32:09 freddy77 Exp $";
 static void *no_unused_tds_sysdep_public_h_warn[] = { rcsid_tds_sysdep_public_h, no_unused_tds_sysdep_public_h_warn };
 
 #ifdef __cplusplus
@@ -28,8 +28,8 @@ extern "C"
 {
 #endif
 
-#include <windows.h>
 #include <winsock2.h>
+#include <windows.h>
 #define tds_sysdep_int16_type short	/* 16-bit int */
 #define tds_sysdep_int32_type int	/* 32-bit int */
 #define tds_sysdep_int64_type __int64	/* 64-bit int */
