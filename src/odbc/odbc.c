9f8599d1a093657f39e05ab15b1484ba324017af
Frediano Ziglio <freddy77@gmail.com>
2012-05-13 13:27:13 +0100

    fix buffer overflow parsing column name

--- src/odbc/odbc.c
+++ src/odbc/odbc.c
@@ -2987,7 +2987,7 @@ odbc_populate_ird(TDS_STMT * stmt)
 			int i;
 
 			p = col->table_name;
-			for (i = 0; i < 4; ++i) {
+			for (i = 0; ; ++i) {
 				const char *pend;
 
 				if (*p == '[' || *p == '\"') {
@@ -3000,11 +3000,12 @@ odbc_populate_ird(TDS_STMT * stmt)
 				partials[i].start = p;
 				partials[i].end = pend;
 				p = pend;
-				if (*p != '.')
+				if (i == 3 || *p != '.')
 					break;
 				++p;
 			}
 
+			/* here i points to last element */
 			odbc_unquote(buf, sizeof(buf), partials[i].start, partials[i].end);
 			tds_dstr_copy(&drec->sql_desc_base_table_name, buf);
 
