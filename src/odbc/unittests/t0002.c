dd3f2d006abd1e33ad384f7242b9359f457ad1f1
Frediano Ziglio <freddy77@gmail.com>
2012-05-20 12:18:14 +0100

    style change

--- src/odbc/unittests/t0002.c
+++ src/odbc/unittests/t0002.c
@@ -3,12 +3,13 @@
 static char software_version[] = "$Id: t0002.c,v 1.17 2010-07-05 09:20:33 freddy77 Exp $";
 static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
 
-#define SWAP_STMT(b) do { SQLHSTMT xyz = odbc_stmt; odbc_stmt = b; b = xyz; } while(0)
+#define SWAP_STMT() do { SQLHSTMT xyz = odbc_stmt; \
+	odbc_stmt = old_odbc_stmt; old_odbc_stmt = xyz; } while(0)
 
 int
 main(int argc, char *argv[])
 {
-	HSTMT old_odbc_stmt;
+	HSTMT old_odbc_stmt = SQL_NULL_HSTMT;
 
 	odbc_connect();
 
@@ -22,8 +23,7 @@ main(int argc, char *argv[])
 	 * then make another query with first select and drop this statement
 	 * result should not disappear (required for DBD::ODBC)
 	 */
-	old_odbc_stmt = odbc_stmt;
-	odbc_stmt = SQL_NULL_HSTMT;
+	SWAP_STMT();
 	CHKAllocStmt(&odbc_stmt, "S");
 
 	odbc_command("select * from #odbctestdata where 0=1");
@@ -32,13 +32,14 @@ main(int argc, char *argv[])
 
 	CHKCloseCursor("SI");
 
-	SWAP_STMT(old_odbc_stmt);
+	SWAP_STMT();
 	odbc_command("select * from #odbctestdata");
-	SWAP_STMT(old_odbc_stmt);
+	SWAP_STMT();
 
 	/* drop first statement .. data should not disappear */
 	CHKFreeStmt(SQL_DROP, "S");
-	odbc_stmt = old_odbc_stmt;
+	odbc_stmt = SQL_NULL_HSTMT;
+	SWAP_STMT();
 
 	CHKFetch("SI");
 
