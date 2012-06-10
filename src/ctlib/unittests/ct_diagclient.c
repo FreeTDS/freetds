2580892d6f2915b9e72eca6c2a013d270d9e649d
Frediano Ziglio <freddy77@gmail.com>
2012-06-10 09:29:22 +0100

    fix more overflow in ctlib tests

--- src/ctlib/unittests/ct_diagclient.c
+++ src/ctlib/unittests/ct_diagclient.c
@@ -26,7 +26,7 @@ main(int argc, char *argv[])
 	CS_INT num_cols;
 
 	CS_DATAFMT datafmt;
-	CS_INT datalength;
+	CS_INT datalength[2];
 	CS_SMALLINT ind[2];
 	CS_INT count, row_count = 0;
 	CS_INT cv;
@@ -122,7 +122,7 @@ main(int argc, char *argv[])
 
 			datafmt.count = 2;
 
-			ret = ct_bind(cmd, 1, &datafmt, &col1, &datalength, ind);
+			ret = ct_bind(cmd, 1, &datafmt, &col1[0], datalength, ind);
 			if (ret != CS_SUCCEED) {
 				fprintf(stderr, "ct_bind() failed\n");
 				return 1;
@@ -138,7 +138,7 @@ main(int argc, char *argv[])
 			datafmt.maxlength = 5;
 			datafmt.count = 4;
 
-			ret = ct_bind(cmd, 2, &datafmt, col2[0], &datalength, ind);
+			ret = ct_bind(cmd, 2, &datafmt, &col2[0], datalength, ind);
 			if (ret != CS_SUCCEED) {
 
 				if (ct_diag(conn, CS_STATUS, CS_CLIENTMSG_TYPE, CS_UNUSED, &num_msgs) != CS_SUCCEED) {
@@ -181,7 +181,7 @@ main(int argc, char *argv[])
 				return 1;
 			}
 			datafmt.count = 2;
-			ret = ct_bind(cmd, 2, &datafmt, col2[0], &datalength, ind);
+			ret = ct_bind(cmd, 2, &datafmt, &col2[0], datalength, ind);
 			if (ret != CS_SUCCEED) {
 				fprintf(stderr, "ct_bind() failed\n");
 				return 1;
