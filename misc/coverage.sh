9df976d6a2b76f726f638df8be06e81940c9a7c7
Frediano Ziglio <freddy77@gmail.com>
2012-10-19 09:02:31 +0100

    Avoid full path on coverage information

--- misc/coverage.sh
+++ misc/coverage.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # build coverage output
 # require lcov installed
 
 set -e
+set -o pipefail
 
 # go to main distro dir
+trap 'echo Error at line $LINENO' ERR
 DIR=`dirname $0`
 cd "$DIR/.."
+DIR=`pwd -P`
+cd "$DIR"
 
 # cleanup
 rm -rf covtmp coverage
@@ -15,16 +19,16 @@ mkdir covtmp
 mkdir coverage
 
 # move required files in covtmp
-find include/ src/ \( -name \*.\[ch\] -o -name \*.da -o -name \*.bb\* -o -name \*.gc\* \) | tar cf -  -T - | (cd covtmp; tar xf -)
+find include/ src/ \( -name \*.\[ch\] -o -name \*.da -o -name \*.bb\* -o -name \*.gc\* \) | tar cf -  -T - | (cd covtmp && exec tar xf -)
 
 # prepare
 cd covtmp
-find -name \*.bb\* | grep .libs | xargs rm -f
+find -name \*.bb\* | grep .libs | xargs rm -f || true
 find -name \*.da -o -name \*.gc\* | grep '/.libs/' | xargs -imao -n1 sh -c 'mv mao $(echo mao | sed s,/.libs/,/,g)'
 
 # generate coverage
 geninfo . -o out0.info -t 'Test'
-perl -ne '$skip = 1 if (m(^SF:/usr/include/sys/)); print if(!$skip); $skip = 0 if (/^end_of_record$/);' < out0.info > out.info
-genhtml out.info -t 'FreeTDS coverage' -o ../coverage
+perl -ne '$skip = 1 if (m(^SF:/usr/include/)); print if(!$skip); $skip = 0 if (/^end_of_record$/);' < out0.info > out.info
+genhtml out.info -t 'FreeTDS coverage' -o ../coverage -p "$DIR/covtmp"
 
 echo Success !!
