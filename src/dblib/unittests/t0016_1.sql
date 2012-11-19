3bf33aaad3256016da3b023783068dee4d4f6e8b
Frediano Ziglio <freddy77@gmail.com>
2012-11-19 19:15:03 +0000

    Fix bcp test for Sybase servers
    Second test did not work cause it required MS nvarchar so adding
    uniqueidentifier make it work with Sybase.

--- src/dblib/unittests/t0016_1.sql
+++ src/dblib/unittests/t0016_1.sql
@@ -1,4 +1,4 @@
-create table #dblib0016 (f1 int not null, str nvarchar(5))
+create table #dblib0016 (f1 int not null, str nvarchar(5), uuid uniqueidentifier)
 go
 select * from #dblib0016 where 0=1
 go
