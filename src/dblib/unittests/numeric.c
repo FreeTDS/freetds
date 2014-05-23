e2817f00bdf28840ec213bf734af097a2e6f64a6
Frediano Ziglio <freddy77@gmail.com>
2014-05-23 10:45:29 +0100

    Better tests for numeric due to improvement of dbbind
    Now dbbind take into considerations differences between MS and Sybase
    implementations.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/dblib/unittests/numeric.c
+++ src/dblib/unittests/numeric.c
@@ -15,7 +15,7 @@ dump_addr(FILE *out, const char *msg, const void *p, size_t len)
 static void
 chk(RETCODE ret, const char *msg)
 {
-	printf("res %d\n", ret);
+	printf("%s: res %d\n", msg, ret);
 	if (ret == SUCCEED)
 		return;
 	fprintf(stderr, "error: %s\n", msg);
@@ -23,15 +23,31 @@ chk(RETCODE ret, const char *msg)
 }
 
 static void
-test(int bind_type)
+zero_end(DBNUMERIC *num)
+{
+	/* 27213 == math.floor(math.log(10,256)*65536) */
+	unsigned len = 4u+num->precision*27213u/65536u;
+	if (num->precision < 1 || num->precision > 77)
+		return;
+	assert(len >= 4 && len <= sizeof(*num));
+	memset(((char*) num) + len, 0, sizeof(*num) - len);
+}
+
+static int msdblib;
+
+static void
+test(int bind_type, const char *bind_name, int override_prec, int override_scale, int out_prec, int out_scale)
 {
 	LOGINREC *login;
 	DBPROCESS *dbproc;
 	DBNUMERIC *num = NULL, *num2 = NULL;
 	RETCODE ret;
+	DBTYPEINFO ti;
 	int i;
 
-	sql_rewind();
+	printf("*** Starting test msdblib %d bind %s prec %d scale %d out prec %d out scale %d\n",
+		msdblib, bind_name, override_prec, override_scale, out_prec, out_scale);
+	chk(sql_rewind(), "sql_rewind");
 	login = dblogin();
 
 	DBSETLUSER(login, USER);
@@ -40,7 +56,7 @@ test(int bind_type)
 	dbsetmaxprocs(25);
 	DBSETLHOST(login, SERVER);
 
-	dbproc = dbopen(login, SERVER);
+	dbproc = tdsdbopen(login, SERVER, msdblib);
 	dbloginfree(login);
 	login = NULL;
 	if (strlen(DATABASE))
@@ -81,9 +97,10 @@ test(int bind_type)
 	chk(ret, "dbrpcinit");
 
 	num = (DBDECIMAL *) calloc(1, sizeof(DBDECIMAL));
-	num->scale = 5;
-	num->precision = 16;
-	dbconvert(dbproc, SYBVARCHAR, (const BYTE *) "123.45", -1, SYBDECIMAL, (BYTE *) num, sizeof(*num));
+	ti.scale = 5;
+	ti.precision = 16;
+	ret = dbconvert_ps(dbproc, SYBVARCHAR, (const BYTE *) "123.45", -1, SYBDECIMAL, (BYTE *) num, sizeof(*num), &ti);
+	chk(ret > 0, "dbconvert_ps");
 
 	ret = dbrpcparam(dbproc, "@idecimal", 0, SYBDECIMAL, -1, sizeof(DBDECIMAL), (BYTE *) num);
 	chk(ret, "dbrpcparam");
@@ -94,9 +111,10 @@ test(int bind_type)
 
 	/* TODO check MS/Sybase format */
 	num2 = (DBDECIMAL *) calloc(1, sizeof(DBDECIMAL));
-	num2->precision = 20;
-	num2->scale = 10;
-	dbconvert(dbproc, SYBVARCHAR, (const BYTE *) "246.9", -1, SYBDECIMAL, (BYTE *) num2, sizeof(*num2));
+	ti.precision = override_prec;
+	ti.scale     = override_scale;
+	ret = dbconvert_ps(dbproc, SYBVARCHAR, (const BYTE *) "246.9", -1, SYBDECIMAL, (BYTE *) num2, sizeof(*num2), &ti);
+	chk(ret > 0, "dbconvert_ps");
 
 	for (i=0; (ret = dbresults(dbproc)) != NO_MORE_RESULTS; ++i) {
 		RETCODE row_code;
@@ -109,16 +127,18 @@ test(int bind_type)
 			printf("dbrows() returned SUCCEED, processing rows\n");
 
 			memset(num, 0, sizeof(*num));
-			num->precision = num2->precision;
-			num->scale = num2->scale;
+			num->precision = out_prec  ? out_prec  : num2->precision;
+			num->scale     = out_scale ? out_scale : num2->scale;
 			dbbind(dbproc, 1, bind_type, 0, (BYTE *) num);
 
 			while ((row_code = dbnextrow(dbproc)) != NO_MORE_ROWS) {
 				if (row_code == REG_ROW) {
+					zero_end(num);
+					zero_end(num2);
 					if (memcmp(num, num2, sizeof(*num)) != 0) {
 						fprintf(stderr, "Failed. Output results does not match\n");
-						dump_addr(stderr, "numeric: ", num, sizeof(*num));
-						dump_addr(stderr, "numeric2:", num2, sizeof(*num2));
+						dump_addr(stderr, "got:      ", num, sizeof(*num));
+						dump_addr(stderr, "expected: ", num2, sizeof(*num2));
 						exit(1);
 					}
 				} else {
@@ -145,6 +165,8 @@ test(int bind_type)
 	dbclose(dbproc);
 }
 
+#define test(a,b,c,d,e) test(a, #a, b, c, d, e)
+
 int
 main(int argc, char **argv)
 {
@@ -152,8 +174,45 @@ main(int argc, char **argv)
 
 	dbinit();
 
-	test(DECIMALBIND);
-	test(NUMERICBIND);
+	/* tests with MS behaviour */
+	msdblib = 1;
+	test(DECIMALBIND,    20, 10, 0, 0);
+	test(NUMERICBIND,    20, 10, 0, 0);
+	test(SRCDECIMALBIND, 20, 10, 0, 0);
+	test(SRCNUMERICBIND, 20, 10, 0, 0);
+	/* in these 2 case MS override what server returns */
+	test(DECIMALBIND,    10,  4, 10, 4);
+	test(NUMERICBIND,    10,  4, 10, 4);
+	test(SRCDECIMALBIND, 20, 10, 10, 4);
+	test(SRCNUMERICBIND, 20, 10, 10, 4);
+
+	/* tests with Sybase behaviour */
+	msdblib = 0;
+	test(DECIMALBIND,    20, 10, 0, 0);
+	test(NUMERICBIND,    20, 10, 0, 0);
+	test(SRCDECIMALBIND, 20, 10, 0, 0);
+	test(SRCNUMERICBIND, 20, 10, 0, 0);
+	/* no matter what Sybase return always according to source */
+	test(DECIMALBIND,    20, 10, 10, 4);
+	test(NUMERICBIND,    20, 10, 10, 4);
+	test(SRCDECIMALBIND, 20, 10, 10, 4);
+	test(SRCNUMERICBIND, 20, 10, 10, 4);
+
+	chk(sql_reopen("numeric_2"), "sql_reopen");
+
+	msdblib = 1;
+	/* on MS use always output */
+	test(DECIMALBIND,    20,  0, 20, 0);
+	test(NUMERICBIND,    19,  0, 19, 0);
+	test(SRCDECIMALBIND, 18,  0, 18, 0);
+	test(SRCNUMERICBIND, 17,  0, 17, 0);
+
+	msdblib = 0;
+	test(DECIMALBIND,    18,  0, 20, 0);
+	test(NUMERICBIND,    18,  0, 19, 0);
+	/* this is MS only and behave like MS */
+	test(SRCDECIMALBIND, 18,  0, 18, 0);
+	test(SRCNUMERICBIND, 17,  0, 17, 0);
 
 	dbexit();
 
