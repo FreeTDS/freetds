184b3d2510db0561c9e76693eaeb27d8aad06764
Frediano Ziglio <freddy77@gmail.com>
2014-05-26 00:44:56 +0100

    Constify dashes
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/tds/token.c
+++ src/tds/token.c
@@ -1603,7 +1603,7 @@ tds7_process_result(TDSSOCKET * tds)
 	}
 		
 	if (num_cols > 0) {
-		static char dashes[31] = "------------------------------";
+		static const char dashes[31] = "------------------------------";
 		tdsdump_log(TDS_DBG_INFO1, " %-20s %-15s %-15s %-7s\n", "name", "size/wsize", "type/wtype", "utype");
 		tdsdump_log(TDS_DBG_INFO1, " %-20s %15s %15s %7s\n", dashes+10, dashes+30-15, dashes+30-15, dashes+30-7);
 	}
