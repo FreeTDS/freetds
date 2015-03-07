50fb8b4851dab36ddeb0f89231a4f3e417856e3a
Frediano Ziglio <freddy77@gmail.com>
2015-03-07 10:01:49 +0000

    Add binaries to t0016 test
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/dblib/unittests/t0016_11.sql
+++ src/dblib/unittests/t0016_11.sql
@@ -1,4 +1,4 @@
-create table #dblib0016 (id int not null, var uniqueidentifier null, c1 char(20) not null, v1 varchar(20) not null, c2 char(20) null, v2 varchar(20) null)
+create table #dblib0016 (id int not null, var uniqueidentifier null, c1 char(20) not null, v1 varchar(20) not null, c2 char(20) null, v2 varchar(20) null, b1 binary(20) null, vb1 varbinary(20) null, img image null, txt text null)
 go
 select * from #dblib0016 where 0=1
 go
