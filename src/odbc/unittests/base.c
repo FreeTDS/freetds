22f01379442ff145697414d459a53049e8cbc776
Frediano Ziglio <freddy77@gmail.com>
2015-05-18 21:20:18 +0100

    Remove CVS Id from base example
    Not using CVS anymore.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/unittests/base.c
+++ src/odbc/unittests/base.c
@@ -2,9 +2,6 @@
 
 /* TODO place comment here */
 
-static char software_version[] = "$Id: base.c,v 1.2 2010-07-05 09:20:32 freddy77 Exp $";
-static void *no_unused_var_warn[] = { software_version, no_unused_var_warn };
-
 int
 main(int argc, char *argv[])
 {
