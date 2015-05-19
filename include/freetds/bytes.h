0165439705ef44cbd20f298a33cd6615b3e9f07d
Frediano Ziglio <freddy77@gmail.com>
2015-05-19 14:29:03 +0100

    Improve inline asm for Sparc
    Remove volatile clause in inline assembly as it could reduce code
    optmization for no reasons.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- include/freetds/bytes.h
+++ include/freetds/bytes.h
@@ -208,14 +208,14 @@ static inline TDS_USMALLINT
 TDS_GET_UA2LE(void *ptr)
 {
 	unsigned long res;
-	__asm__ __volatile__ ("lhbrx %0,0,%1\n" : "=r" (res) : "r" (ptr), "m"(*(TDS_USMALLINT*)ptr));
+	__asm__ ("lhbrx %0,0,%1\n" : "=r" (res) : "r" (ptr), "m"(*(TDS_USMALLINT*)ptr));
 	return (TDS_USMALLINT) res;
 }
 static inline TDS_UINT
 TDS_GET_UA4LE(void *ptr)
 {
 	unsigned long res;
-	__asm__ __volatile__ ("lwbrx %0,0,%1\n" : "=r" (res) : "r" (ptr), "m"(*(TDS_UINT*)ptr));
+	__asm__ ("lwbrx %0,0,%1\n" : "=r" (res) : "r" (ptr), "m"(*(TDS_UINT*)ptr));
 	return (TDS_UINT) res;
 }
 
@@ -224,12 +224,12 @@ TDS_GET_UA4LE(void *ptr)
 static inline void
 TDS_PUT_UA2LE(void *ptr, unsigned data)
 {
-    asm volatile ("sthbrx %1,0,%2\n" : "=m" (*(TDS_USMALLINT *)ptr) : "r" (data), "r" (ptr));
+    __asm__ ("sthbrx %1,0,%2\n" : "=m" (*(TDS_USMALLINT *)ptr) : "r" (data), "r" (ptr));
 }
 static inline void
 TDS_PUT_UA4LE(void *ptr, unsigned data)
 {
-    asm volatile ("stwbrx %1,0,%2\n" : "=m" (*(TDS_UINT *)ptr) : "r" (data), "r" (ptr));
+    __asm__ ("stwbrx %1,0,%2\n" : "=m" (*(TDS_UINT *)ptr) : "r" (data), "r" (ptr));
 }
 #endif
 
