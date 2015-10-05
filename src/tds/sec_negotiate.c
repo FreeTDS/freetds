a38ff5a19d031217b4c7f1baea5d00da84d9aeec
Frediano Ziglio <freddy77@gmail.com>
2015-10-05 21:41:05 +0100

    Supports Sybase encrypted logins even with former GnuTLS
    If Nettle is not supported use Gcrypt instead.
    This require to support some DER parsing (using Tasn1
    library directly) and emulating some Gmp functions
    (used by Nettle) using Gcrypt functions.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/tds/sec_negotiate.c
+++ src/tds/sec_negotiate.c
@@ -40,15 +40,27 @@
 #include "replacements.h"
 
 #ifdef HAVE_GNUTLS
-#include <gnutls/gnutls.h>
-#include <gnutls/crypto.h>
-#include <gnutls/abstract.h>
+#  include <gnutls/gnutls.h>
+#  include <gnutls/crypto.h>
+#  ifdef HAVE_GNUTLS_ABSTRACT_H
+#    include <gnutls/abstract.h>
+#  endif
 
-#include <nettle/asn1.h>
-#include <nettle/rsa.h>
-#include <nettle/bignum.h>
+#  if !defined(HAVE_NETTLE) || !defined(HAVE_GMP) || !defined(HAVE_GNUTLS_RND)
+#    include <gcrypt.h>
+#  endif
+
+#  ifndef HAVE_NETTLE
+#    include <libtasn1.h>
+#  endif
+
+#  ifdef HAVE_NETTLE
+#    include <nettle/asn1.h>
+#    include <nettle/rsa.h>
+#    include <nettle/bignum.h>
+#  endif
 #elif defined(HAVE_OPENSSL)
-#include <openssl/rand.h>
+#  include <openssl/rand.h>
 #endif
 
 
@@ -63,14 +75,135 @@
  * @{
  */
 
-#if defined(HAVE_NETTLE) && defined(HAVE_GMP)
 
-static void
-rnd_func(void *ctx, unsigned len, uint8_t * out)
+/* emulate GMP if not present */
+#if !defined(HAVE_GMP) && defined(HAVE_GNUTLS)
+#define HAVE_GMP 1
+
+typedef struct {
+	gcry_mpi_t num;
+} mpz_t[1];
+
+#define mpz_powm(w,n,e,m) \
+	gcry_mpi_powm((w)->num, (n)->num, (e)->num, (m)->num);
+#define mpz_init(n) do { (n)->num = NULL; } while(0)
+#define mpz_clear(n) gcry_mpi_release((n)->num)
+
+#endif
+
+
+/* emulate Nettle is not present */
+#if !defined(HAVE_NETTLE) && defined(HAVE_GNUTLS)
+#define HAVE_NETTLE 1
+
+typedef void nettle_random_func(void *ctx, unsigned len, uint8_t *out);
+
+static inline void
+nettle_mpz_set_str_256_u(mpz_t x, unsigned length, const uint8_t *s)
 {
-	tds_random_buffer(out, len);
+	gcry_mpi_scan(&x->num, GCRYMPI_FMT_USG, s, length, NULL);
 }
 
+static inline void
+nettle_mpz_get_str_256(unsigned length, uint8_t *s, const mpz_t x)
+{
+	gcry_mpi_print(GCRYMPI_FMT_USG, s, length, NULL, x->num);
+}
+
+struct asn1_der_iterator {
+	const unsigned char *data, *data_end;
+	unsigned long length;
+	unsigned long type;
+};
+
+enum asn1_iterator_result {
+	ASN1_ITERATOR_ERROR,
+	ASN1_ITERATOR_PRIMITIVE,
+	ASN1_ITERATOR_CONSTRUCTED,
+	ASN1_ITERATOR_END,
+};
+
+enum {
+	ASN1_SEQUENCE = ASN1_TAG_SEQUENCE,
+};
+
+static enum asn1_iterator_result
+asn1_der_iterator_next(struct asn1_der_iterator *der)
+{
+	unsigned char cls;
+	unsigned long tag;
+	int len;
+	long l;
+
+	if (asn1_get_tag_der(der->data, der->data_end - der->data, &cls, &len, &tag) != ASN1_SUCCESS)
+		return ASN1_ITERATOR_ERROR;
+	der->type = tag;
+	der->data += len;
+	l = asn1_get_length_der(der->data, der->data_end - der->data, &len);
+	if (l < 0)
+		return ASN1_ITERATOR_ERROR;
+	der->data += len;
+	der->length = l;
+	if (cls == ASN1_CLASS_STRUCTURED)
+		return ASN1_ITERATOR_CONSTRUCTED;
+	return ASN1_ITERATOR_PRIMITIVE;
+}
+
+static enum asn1_iterator_result
+asn1_der_iterator_first(struct asn1_der_iterator *der, int size, const void *der_buf)
+{
+	der->data = (const unsigned char *) der_buf;
+	der->data_end = der->data + size;
+
+	return asn1_der_iterator_next(der);
+}
+
+struct rsa_public_key {
+	unsigned size;
+	mpz_t n, e;
+};
+
+static void
+rsa_public_key_init(struct rsa_public_key *key)
+{
+	key->size = 0;
+	mpz_init(key->n);
+	mpz_init(key->e);
+}
+
+static void
+rsa_public_key_clear(struct rsa_public_key *key)
+{
+	mpz_clear(key->n);
+	mpz_clear(key->e);
+}
+
+static int
+rsa_public_key_from_der_iterator(struct rsa_public_key *key, unsigned key_bits, struct asn1_der_iterator *der)
+{
+	enum asn1_iterator_result ret;
+
+	ret = asn1_der_iterator_next(der);
+	if (ret != ASN1_ITERATOR_PRIMITIVE || der->type != ASN1_TAG_INTEGER)
+		return 0;
+	gcry_mpi_scan(&key->n->num, GCRYMPI_FMT_USG, der->data, der->length, NULL);
+	key->size = (gcry_mpi_get_nbits(key->n->num)+7)/8;
+	der->data += der->length;
+
+	ret = asn1_der_iterator_next(der);
+	if (ret != ASN1_ITERATOR_PRIMITIVE || der->type != ASN1_TAG_INTEGER)
+		return 0;
+	gcry_mpi_scan(&key->e->num, GCRYMPI_FMT_USG, der->data, der->length, NULL);
+
+	return 1;
+}
+
+static void
+sha1(uint8_t *hash, const void *data, size_t len)
+{
+	gcry_md_hash_buffer(GCRY_MD_SHA1, hash, data, len);
+}
+#elif defined(HAVE_NETTLE)
 static void
 sha1(uint8_t *hash, const void *data, size_t len)
 {
@@ -78,6 +211,16 @@ sha1(uint8_t *hash, const void *data, size_t len)
 	sha1_init(&ctx);
 	sha1_update(&ctx, len, (const uint8_t *) data);
 	sha1_digest(&ctx, 20, hash);
+}
+#endif
+
+
+#if defined(HAVE_NETTLE) && defined(HAVE_GMP)
+
+static void
+rnd_func(void *ctx, unsigned len, uint8_t * out)
+{
+	tds_random_buffer(out, len);
 }
 
 #define dumpl(b,l) tdsdump_dump_buf(TDS_DBG_INFO1, #b, b, l)
