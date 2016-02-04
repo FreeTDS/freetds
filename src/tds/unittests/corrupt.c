65d9a6aad58c2ee5894c94958e81be319320e742
Frediano Ziglio <freddy77@gmail.com>
2016-02-04 22:15:22 +0000

    test a bug causing packet corruption
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/tds/unittests/corrupt.c
+++ src/tds/unittests/corrupt.c
@@ -26,6 +26,64 @@
 #include "common.h"
 #include <freetds/checks.h>
 
+static const char select_query[] = "\nselect 'test'";
+
+static void
+unfinished_query_test(TDSSOCKET *tds)
+{
+	int char_len;
+	char *buf, *p;
+	int i, len;
+	union {
+		TDS_USMALLINT si;
+		TDS_UINT i;
+		TDS_INT8 i8;
+		char buf[8];
+	} conv;
+
+	tds_init_write_buf(tds);
+
+	/* try to build an invalid (unfinished) query split in two packets */
+	char_len = IS_TDS7_PLUS(tds->conn) ? 2 : 1;
+	buf = calloc(1, tds->out_buf_max + 200);
+	memset(buf, '-', tds->out_buf_max + 200);
+	strcpy(buf + (tds->out_buf_max - 8) / char_len - strlen(select_query) + 1, select_query);
+	memset(strchr(buf, 0), 0, 16);
+
+	/* convert if needed */
+	len = strlen(buf);
+	for (i = len; --i >= 0; ) {
+		char c = buf[i];
+		buf[i * char_len + 0] = c;
+		if (IS_TDS7_PLUS(tds->conn))
+			buf[i * char_len + 1] = 0;
+	}
+	len *= char_len;
+
+	/* send the query using tds_put_int8, non allineati */
+	tds->out_flag = TDS_QUERY;
+	if (tds_set_state(tds, TDS_WRITING) != TDS_WRITING)
+		exit(1);
+	p = buf;
+	memcpy(conv.buf, p, 2);
+	tds_put_smallint(tds, conv.si);
+	p += 2;
+	for (; p < buf + len; p += 8) {
+		CHECK_TDS_EXTRA(tds);
+		memcpy(conv.buf, p, 8);
+		tds_put_int8(tds, conv.i8);
+	}
+	tds_flush_packet(tds);
+	tds_set_state(tds, TDS_PENDING);
+
+	/* check result was fine */
+	if (TDS_FAILED(tds_process_simple_query(tds))) {
+		fprintf(stderr, "Error in prepared query\n");
+		exit(1);
+	}
+	free(buf);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -42,6 +100,8 @@ main(int argc, char **argv)
 		fprintf(stderr, "try_tds_login() failed\n");
 		return 1;
 	}
+
+	unfinished_query_test(tds);
 
 	tds->out_flag = TDS_QUERY;
 	if (tds_set_state(tds, TDS_WRITING) != TDS_WRITING) {
