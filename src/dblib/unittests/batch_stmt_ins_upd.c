f8c630ec9938da710ab36e4d39aaaecfa2a93843
Jeff Farr <jefarr@wayfair.com>
2016-11-22 13:19:46 -0500

    changed comments to C style and refactored fprintf to printf

--- src/dblib/unittests/batch_stmt_ins_upd.c
+++ src/dblib/unittests/batch_stmt_ins_upd.c
@@ -17,184 +17,184 @@ static int failed = 0;
 int
 main(int argc, char **argv)
 {
-  const int rows_to_add = 50;
-  LOGINREC *login;
-  DBPROCESS *dbproc;
-  int i;
-  char teststr[1024];
-  DBINT testint, erc;
+	LOGINREC *login;
+	DBPROCESS *dbproc;
+	int i;
+	DBINT erc;
 
-  set_malloc_options();
+	set_malloc_options();
 
-  read_login_info(argc, argv);
-  if (argc > 1) {
-    argc -= optind;
-    argv += optind;
-  }
+	read_login_info(argc, argv);
+	if (argc > 1) {
+		argc -= optind;
+		argv += optind;
+	}
 
-  fprintf(stdout, "Starting %s\n", argv[0]);
+	printf("Starting %s\n", argv[0]);
 
-  /* Fortify_EnterScope(); */
-  dbinit();
+	/* Fortify_EnterScope(); */
+	dbinit();
 
-  dberrhandle(syb_err_handler);
-  dbmsghandle(syb_msg_handler);
+	dberrhandle(syb_err_handler);
+	dbmsghandle(syb_msg_handler);
 
-  fprintf(stdout, "About to logon as \"%s\"\n", USER);
+	printf("About to logon as \"%s\"\n", USER);
 
-  login = dblogin();
-  DBSETLPWD(login, PASSWORD);
-  DBSETLUSER(login, USER);
-  DBSETLAPP(login, "wf_dbresults");
+	login = dblogin();
+	DBSETLPWD(login, PASSWORD);
+	DBSETLUSER(login, USER);
+	DBSETLAPP(login, "wf_dbresults");
 
-  if (argc > 1) {
-    printf("server and login timeout overrides (%s and %s) detected\n", argv[0], argv[1]);
-    strcpy(SERVER, argv[0]);
-    i = atoi(argv[1]);
-    if (i) {
-      i = dbsetlogintime(i);
-      printf("dbsetlogintime returned %s.\n", (i == SUCCEED)? "SUCCEED" : "FAIL");
-    }
-  }
+	if (argc > 1) {
+		printf("server and login timeout overrides (%s and %s) detected\n", argv[0], argv[1]);
+		strcpy(SERVER, argv[0]);
+		i = atoi(argv[1]);
+		if (i) {
+			i = dbsetlogintime(i);
+			printf("dbsetlogintime returned %s.\n", (i == SUCCEED)? "SUCCEED" : "FAIL");
+		}
+	}
 
-  fprintf(stdout, "About to open \"%s\"\n", SERVER);
+	printf("About to open \"%s\"\n", SERVER);
 
-  dbproc = dbopen(login, SERVER);
-  if (!dbproc) {
-    fprintf(stderr, "Unable to connect to %s\n", SERVER);
-    return 1;
-  }
-  dbloginfree(login);
+	dbproc = dbopen(login, SERVER);
+	if (!dbproc) {
+		fprintf(stderr, "Unable to connect to %s\n", SERVER);
+		return 1;
+	}
+	dbloginfree(login);
 
-  fprintf(stdout, "Using database \"%s\"\n", DATABASE);
-  if (strlen(DATABASE)) {
-    erc = dbuse(dbproc, DATABASE);
-    assert(erc == SUCCEED);
-  }
+	printf("Using database \"%s\"\n", DATABASE);
+	if (strlen(DATABASE)) {
+		erc = dbuse(dbproc, DATABASE);
+		assert(erc == SUCCEED);
+	}
 
-  RETCODE ret;
-  int rowcount;
-  int colcount;
+	RETCODE ret;
+	int rowcount;
+	int colcount;
 
-  /*
-  * This test is written to simulate how dblib is used in PDO
-  * functions are called in the same order they would be if doing
-  * PDO::query followed by some number of PDO::statement->nextRowset
-  */
+ /*
+	* This test is written to simulate how dblib is used in PDO
+	* functions are called in the same order they would be if doing
+	* PDO::query followed by some number of PDO::statement->nextRowset
+	*/
 
-  // First, call everything that happens in PDO::query
-  // this will return the results of the CREATE TABLE statement
-  dbcancel(dbproc);
+	/*
+	 * First, call everything that happens in PDO::query
+	 * this will return the results of the CREATE TABLE statement
+	 */
+	dbcancel(dbproc);
 
-  fprintf(stdout, "using sql_cmd\n");
-  sql_cmd(dbproc);
-  dbsqlexec(dbproc);
+	printf("using sql_cmd\n");
+	sql_cmd(dbproc);
+	dbsqlexec(dbproc);
 
-  ret = dbresults(dbproc);
-  rowcount = DBCOUNT(dbproc);
-  colcount = dbnumcols(dbproc);
+	ret = dbresults(dbproc);
+	rowcount = DBCOUNT(dbproc);
+	colcount = dbnumcols(dbproc);
 
-  fprintf(stdout, "RETCODE: %d\n", ret);
-  fprintf(stdout, "ROWCOUNT: %d\n", rowcount);
-  fprintf(stdout, "COLCOUNT: %d\n\n", colcount);
+	printf("RETCODE: %d\n", ret);
+	printf("ROWCOUNT: %d\n", rowcount);
+	printf("COLCOUNT: %d\n\n", colcount);
 
-  // check the results of the create table statement
-  assert(ret == SUCCEED);
-  assert(rowcount == -1);
-  assert(colcount == 0);
+	/* check the results of the create table statement */
+	assert(ret == SUCCEED);
+	assert(rowcount == -1);
+	assert(colcount == 0);
 
-  // now simulate calling nextRowset() for each remaining statement in our batch
+	/* now simulate calling nextRowset() for each remaining statement in our batch */
 
-  // --------------------------------------------------------------------------
-  // INSERT
-  // --------------------------------------------------------------------------
-  ret = dbnextrow(dbproc);
-  assert(ret == NO_MORE_ROWS);
+	/*
+	 * INSERT
+	 */
+	ret = dbnextrow(dbproc);
+	assert(ret == NO_MORE_ROWS);
 
-  ret = dbresults(dbproc);
-  rowcount = DBCOUNT(dbproc);
-  colcount = dbnumcols(dbproc);
+	ret = dbresults(dbproc);
+	rowcount = DBCOUNT(dbproc);
+	colcount = dbnumcols(dbproc);
 
-  fprintf(stdout, "RETCODE: %d\n", ret);
-  fprintf(stdout, "ROWCOUNT: %d\n", rowcount);
-  fprintf(stdout, "COLCOUNT: %d\n\n", colcount);
+	printf("RETCODE: %d\n", ret);
+	printf("ROWCOUNT: %d\n", rowcount);
+	printf("COLCOUNT: %d\n\n", colcount);
 
-  assert(ret == SUCCEED);
-  assert(rowcount == 3);
-  assert(colcount == 0);
+	assert(ret == SUCCEED);
+	assert(rowcount == 3);
+	assert(colcount == 0);
 
-  // --------------------------------------------------------------------------
-  // UPDATE
-  // --------------------------------------------------------------------------
-  ret = dbnextrow(dbproc);
-  assert(ret == NO_MORE_ROWS);
+	/*
+	 * UPDATE
+	 */
+	ret = dbnextrow(dbproc);
+	assert(ret == NO_MORE_ROWS);
 
-  ret = dbresults(dbproc);
-  rowcount = DBCOUNT(dbproc);
-  colcount = dbnumcols(dbproc);
+	ret = dbresults(dbproc);
+	rowcount = DBCOUNT(dbproc);
+	colcount = dbnumcols(dbproc);
 
-  fprintf(stdout, "RETCODE: %d\n", ret);
-  fprintf(stdout, "ROWCOUNT: %d\n", rowcount);
-  fprintf(stdout, "COLCOUNT: %d\n\n", colcount);
+	printf("RETCODE: %d\n", ret);
+	printf("ROWCOUNT: %d\n", rowcount);
+	printf("COLCOUNT: %d\n\n", colcount);
 
-  assert(ret == SUCCEED);
-  assert(rowcount == 3);
-  assert(colcount == 0);
+	assert(ret == SUCCEED);
+	assert(rowcount == 3);
+	assert(colcount == 0);
 
-  // --------------------------------------------------------------------------
-  // INSERT
-  // --------------------------------------------------------------------------
-  ret = dbnextrow(dbproc);
-  assert(ret == NO_MORE_ROWS);
+	/*
+	 * INSERT
+	 */
+	ret = dbnextrow(dbproc);
+	assert(ret == NO_MORE_ROWS);
 
-  ret = dbresults(dbproc);
-  rowcount = DBCOUNT(dbproc);
-  colcount = dbnumcols(dbproc);
+	ret = dbresults(dbproc);
+	rowcount = DBCOUNT(dbproc);
+	colcount = dbnumcols(dbproc);
 
-  fprintf(stdout, "RETCODE: %d\n", ret);
-  fprintf(stdout, "ROWCOUNT: %d\n", rowcount);
-  fprintf(stdout, "COLCOUNT: %d\n\n", colcount);
+	printf("RETCODE: %d\n", ret);
+	printf("ROWCOUNT: %d\n", rowcount);
+	printf("COLCOUNT: %d\n\n", colcount);
 
-  assert(ret == SUCCEED);
-  assert(rowcount == 1);
-  assert(colcount == 0);
+	assert(ret == SUCCEED);
+	assert(rowcount == 1);
+	assert(colcount == 0);
 
-  // --------------------------------------------------------------------------
-  // DROP
-  // --------------------------------------------------------------------------
-  ret = dbnextrow(dbproc);
-  assert(ret == NO_MORE_ROWS);
+	/*
+	 * DROP
+	 */
+	ret = dbnextrow(dbproc);
+	assert(ret == NO_MORE_ROWS);
 
-  ret = dbresults(dbproc);
-  rowcount = DBCOUNT(dbproc);
-  colcount = dbnumcols(dbproc);
+	ret = dbresults(dbproc);
+	rowcount = DBCOUNT(dbproc);
+	colcount = dbnumcols(dbproc);
 
-  fprintf(stdout, "RETCODE: %d\n", ret);
-  fprintf(stdout, "ROWCOUNT: %d\n", rowcount);
-  fprintf(stdout, "COLCOUNT: %d\n\n", colcount);
+	printf("RETCODE: %d\n", ret);
+	printf("ROWCOUNT: %d\n", rowcount);
+	printf("COLCOUNT: %d\n\n", colcount);
 
-  assert(ret == SUCCEED);
-  assert(rowcount == -1);
-  assert(colcount == 0);
+	assert(ret == SUCCEED);
+	assert(rowcount == -1);
+	assert(colcount == 0);
 
-  // Call one more time to be sure we get NO_MORE_RESULTS
-  ret = dbnextrow(dbproc);
-  assert(ret == NO_MORE_ROWS);
+	/* Call one more time to be sure we get NO_MORE_RESULTS */
+	ret = dbnextrow(dbproc);
+	assert(ret == NO_MORE_ROWS);
 
-  ret = dbresults(dbproc);
-  rowcount = DBCOUNT(dbproc);
-  colcount = dbnumcols(dbproc);
+	ret = dbresults(dbproc);
+	rowcount = DBCOUNT(dbproc);
+	colcount = dbnumcols(dbproc);
 
-  fprintf(stdout, "RETCODE: %d\n", ret);
-  fprintf(stdout, "ROWCOUNT: %d\n", rowcount);
-  fprintf(stdout, "COLCOUNT: %d\n\n", colcount);
+	printf("RETCODE: %d\n", ret);
+	printf("ROWCOUNT: %d\n", rowcount);
+	printf("COLCOUNT: %d\n\n", colcount);
 
-  assert(ret == NO_MORE_RESULTS);
-  assert(rowcount == -1);
-  assert(colcount == 0);
+	assert(ret == NO_MORE_RESULTS);
+	assert(rowcount == -1);
+	assert(colcount == 0);
 
-  dbexit();
+	dbexit();
 
-  fprintf(stdout, "%s %s\n", __FILE__, (failed ? "failed!" : "OK"));
-  return failed ? 1 : 0;
+	printf("%s %s\n", __FILE__, (failed ? "failed!" : "OK"));
+	return failed ? 1 : 0;
 }
