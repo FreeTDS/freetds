e1a5c83eab64d94124d68ee513e4420434b2ba75
Jeff Farr <jefarr@wayfair.com>
2016-11-28 14:25:52 -0500

    Add unit tests for batch statements
    If you send a batch of statements in a single call to the database, and these statements don't return rows, the dbresults function doesn't process the data correctly. It will loop over all data the first time it is called and return to the calling function that there are no more rows.
    
    Signed-off-by: Jeff Farr jefarr@wayfair.com

---
+++ src/dblib/unittests/batch_stmt_ins_upd.sql
@@ -0,0 +1,15 @@
+create table #batch_stmt_results(id int);
+insert into #batch_stmt_results values(1), (2), (3);
+update #batch_stmt_results set id = 1;
+insert into #batch_stmt_results values(2);
+drop table #batch_stmt_results;
+create table #batch_stmt (id int)
+insert into #batch_stmt values(1)
+insert into #batch_stmt values(2)
+insert into #batch_stmt values(3)
+go
+create table #batch_stmt_results(id int)
+insert into #batch_stmt_results select id from #batch_stmt
+update #batch_stmt_results set id = 1
+insert into #batch_stmt_results values(2)
+drop table #batch_stmt_results
