FreeTDS/freetds

commit f0400f56a34ce13fd3ece82cdcc21177a0715a8d

Use different encryption level for Sybase and MSSQL
For compatibility MSSQL have to default to REQUEST the old
default (OFF) was threated like REQUEST so there is no issue
to change it.
Sybase instead should not use encryption as on Sybase before
version 16 encrypted login is not working unless specifically
configured.

Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

src/tds/login.c
@@ -641,6 +641,8 @@ tds_send_login(TDSSOCKET * tds, TDSLOGIN * login)
 		tdsdump_log(TDS_DBG_ERROR, "Kerberos login not support using TDS 4.x or 5.0\n");
 		return TDS_FAIL;
 	}
+	if (login->encryption_level == TDS_ENCRYPTION_DEFAULT)
+		login->encryption_level = TDS_ENCRYPTION_OFF;
 	if (login->encryption_level != TDS_ENCRYPTION_OFF) {
 		if (IS_TDS42(tds->conn)) {
 			tdsdump_log(TDS_DBG_ERROR, "Encryption not support using TDS 4.x\n");
@@ -701,7 +703,7 @@ tds_send_login(TDSSOCKET * tds, TDSLOGIN * login)
 	tds_put_login_string(tds, lservname, TDS_MAXNAME);
 	if (IS_TDS42(tds->conn)) {
 		tds_put_login_string(tds, tds_dstr_cstr(&login->password), 255);
-	} else if (login->encryption_level) {
+	} else if (login->encryption_level != TDS_ENCRYPTION_OFF) {
 		tds_put_n(tds, NULL, 256);
 	} else {
 		len = (int)tds_dstr_len(&login->password);
@@ -1116,6 +1118,9 @@ tds71_do_login(TDSSOCKET * tds, TDSLOGIN* login)
 
 	assert(start_pos >= 21 && start_pos <= sizeof(buf));
 	assert(buf[start_pos-1] == 0xff);
+
+	if (login->encryption_level == TDS_ENCRYPTION_DEFAULT)
+		login->encryption_level = TDS_ENCRYPTION_REQUEST;
 
 	/*
 	 * fix a problem with mssql2k which doesn't like

include/freetds/tds.h
@@ -332,7 +332,10 @@ typedef union tds_option_arg
 
 
 typedef enum tds_encryption_level {
-	TDS_ENCRYPTION_OFF, TDS_ENCRYPTION_REQUEST, TDS_ENCRYPTION_REQUIRE
+	TDS_ENCRYPTION_DEFAULT,
+	TDS_ENCRYPTION_OFF,
+	TDS_ENCRYPTION_REQUEST,
+	TDS_ENCRYPTION_REQUIRE
 } TDS_ENCRYPTION_LEVEL;
 
 #define TDS_ZERO_FREE(x) do {free((x)); (x) = NULL;} while(0)

commit 0983e7366fd3f8ad200ed36d4eca8d5c40a314ab

Fix unencrypted login with Sybase
Do not declare we support encryption if is disabled.
This regression was introduced in
f0400f56a34ce13fd3ece82cdcc21177a0715a8d ("Use different
encryption level for Sybase and MSSQL").

Signed-off-by: Frediano Ziglio <freddy77@gmail.com>
Reported-by: Fredy Paquet <fredy@opag.ch>

src/tds/login.c
@@ -742,7 +742,7 @@ tds_send_login(TDSSOCKET * tds, const TDSLOGIN * login)
 	/* oldsecure(2), should be zero, used by old software */
 	tds_put_n(tds, NULL, 2);
 	/* seclogin(1) bitmask */
-	tds_put_byte(tds, encryption_level ? TDS5_SEC_LOG_ENCRYPT2|TDS5_SEC_LOG_NONCE : 0);
+	tds_put_byte(tds, encryption_level != TDS_ENCRYPTION_OFF ? TDS5_SEC_LOG_ENCRYPT2|TDS5_SEC_LOG_NONCE : 0);
 	/* secbulk(1)
 	 * halogin(1) type of ha login
 	 * hasessionid(6) id of session to reconnect