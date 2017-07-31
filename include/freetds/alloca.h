8bfb606cad01598656ebb6e262d7d5155fa9545f
Frediano Ziglio <freddy77@gmail.com>
2017-07-31 14:09:54 +0100

    Update alloca.h file
    This file is taken from autoconf documentation.
    Updating remove some warning in some environment (like MinGW).
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- include/freetds/alloca.h
+++ include/freetds/alloca.h
@@ -1,17 +1,26 @@
 /* Cfr https://www.gnu.org/savannah-checkouts/gnu/autoconf/manual/autoconf-2.69/html_node/Particular-Functions.html */
-#if HAVE_ALLOCA_H
-# include <alloca.h>
-#elif defined __GNUC__
-# define alloca __builtin_alloca
-#elif defined _AIX
-# define alloca __alloca
-#elif defined _MSC_VER
-# include <malloc.h>
-# define alloca _alloca
-#else
+#ifdef STDC_HEADERS
+# include <stdlib.h>
 # include <stddef.h>
-# ifdef  __cplusplus
-extern "C"
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
 # endif
+#endif
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif !defined alloca
+# ifdef __GNUC__
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# elif !defined HAVE_ALLOCA
+#  ifdef  __cplusplus
+extern "C"
+#  endif
 void *alloca (size_t);
+# endif
 #endif
