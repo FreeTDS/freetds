9ebd4fa1988f4a015169608e60b78f85aecfb455
Kokan <peter.kokai@balabit.com>
2017-08-30 20:28:50 +0200

    Ctlib: ct_options handle TEXTSIZE command
    Signed-off-by: Kokan <peter.kokai@balabit.com>

--- src/ctlib/ct.c
+++ src/ctlib/ct.c
@@ -3455,7 +3455,6 @@ ct_options(CS_CONNECTION * con, CS_INT action, CS_INT option, CS_VOID * param, C
 	 *      TDS_OPT_CURWRITE
 	 *      TDS_OPT_NATLANG
 	 *      TDS_OPT_ROWCOUNT
-	 *      TDS_OPT_TEXTSIZE
 	 */
 
 	/*
@@ -3589,6 +3588,11 @@ ct_options(CS_CONNECTION * con, CS_INT action, CS_INT option, CS_VOID * param, C
 				return CS_FAIL;
 		}
 		tds_argsize = (action == CS_SET) ? 1 : 0;
+		break;
+	case CS_OPT_TEXTSIZE:
+		tds_option = TDS_OPT_TEXTSIZE;
+		tds_argument.i = *(CS_INT *) param;
+		tds_argsize = (action == CS_SET) ? sizeof(tds_argument.i) : 0;
 		break;
 	case CS_OPT_TRUNCIGNORE:
 		tds_option = TDS_OPT_TRUNCABORT;	/* note inverted sense */
