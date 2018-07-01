2fdaa94aa67e421277273b8d865d54ca29525598
Frediano Ziglio <freddy77@gmail.com>
2018-07-01 08:01:20 +0100

    odbc: Use wsprintf in DllRegisterServer
    No needs to allocate/free memory.
    Also make easier to use always wide version.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/winsetup.c
+++ src/odbc/winsetup.c
@@ -423,11 +423,19 @@ ConfigTranslator(HWND hwndParent, DWORD * pvOption)
 HRESULT WINAPI
 DllRegisterServer(void)
 {
+	static const TCHAR desc_fmt[] = "FreeTDS%c"
+                "APILevel=2%c"
+                "ConnectFunctions=YYN%c"
+                "DriverODBCVer=03.00%c"
+                "FileUsage=0%c"
+                "SQLLevel=2%c"
+                "Setup=%s%c"
+                "Driver=%s%c";
 	TCHAR fn[MAX_PATH], full_fn[MAX_PATH];
+	TCHAR desc[TDS_VECTOR_SIZE(desc_fmt) + TDS_VECTOR_SIZE(full_fn) * 2 + 2];
 	LPTSTR name;
 	WORD len_out;
 	DWORD cnt;
-	char *desc = NULL;
 	BOOL b_res;
 
 	if (!GetModuleFileName(hinstFreeTDS, fn, TDS_VECTOR_SIZE(fn)))
@@ -435,22 +443,10 @@ DllRegisterServer(void)
 	if (!GetFullPathName(fn, TDS_VECTOR_SIZE(full_fn), full_fn, &name) || !name || full_fn == name)
 		return SELFREG_E_CLASS;
 
-	if (asprintf(&desc, "FreeTDS%c"
-		"APILevel=2%c"
-		"ConnectFunctions=YYN%c"
-		"DriverODBCVer=03.00%c"
-		"FileUsage=0%c"
-		"SQLLevel=2%c"
-		"Setup=%s%c"
-		"Driver=%s%c",
-		0, 0, 0, 0, 0, 0,
-		name, 0, name, 0
-		) < 0)
-		return SELFREG_E_CLASS;
+	wsprintf(desc, desc_fmt, 0, 0, 0, 0, 0, 0, name, 0, name, 0);
 	name[-1] = 0;
 
 	b_res = SQLInstallDriverEx(desc, full_fn, fn, TDS_VECTOR_SIZE(fn), &len_out, ODBC_INSTALL_COMPLETE, &cnt);
-	free(desc);
 	if (!b_res)
 		return SELFREG_E_CLASS;
 	return S_OK;
