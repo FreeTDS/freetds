259bfb2dffd680010791d56d3c6e6cf85e313844
Frediano Ziglio <freddy77@gmail.com>
2018-10-19 13:26:01 +0100

    replacements: Reduce indentation
    Also do not check empty string case twice.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/replacements/strsep.c
+++ src/replacements/strsep.c
@@ -42,19 +42,17 @@ tds_strsep(char **stringp, const char *delim)
 	/* A frequent case is when the delimiter string contains only one
 	 * character.  Here we don't need to call the expensive `strpbrk'
 	 * function and instead work using `strchr'.  */
-	if (delim[0] == '\0' || delim[1] == '\0') {
+	if (delim[0] == '\0') {
+		end = NULL;
+	} else if (delim[1] == '\0') {
 		char ch = delim[0];
 
-		if (ch == '\0') {
+		if (*begin == ch)
+			end = begin;
+		else if (*begin == '\0')
 			end = NULL;
-		} else {
-			if (*begin == ch)
-				end = begin;
-			else if (*begin == '\0')
-				end = NULL;
-			else
-				end = strchr(begin + 1, ch);
-		}
+		else
+			end = strchr(begin + 1, ch);
 	} else {
 		/* Find the end of the token.  */
 		end = strpbrk(begin, delim);
