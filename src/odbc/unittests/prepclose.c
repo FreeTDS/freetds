f839466381b9bba477ebea3fa359799e6cef4d6e
Frediano Ziglio <freddy77@gmail.com>
2018-10-31 08:47:28 +0000

    odbc: Run prepclose test even on Windows
    Use shutdown function instead of dup2 to close the connection so
    test become portable to Windows.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/unittests/prepclose.c
+++ src/odbc/unittests/prepclose.c
@@ -28,32 +28,23 @@
  * prepare or execute a query. This should fail and return an error message.
  */
 
-#if HAVE_FSTAT && defined(S_IFSOCK)
+#ifdef _WIN32
+#define SHUT_RDWR SD_BOTH
+#endif
 
 static int
 close_last_socket(void)
 {
 	TDS_SYS_SOCKET max_socket = odbc_find_last_socket();
-	TDS_SYS_SOCKET sockets[2];
 
 	if (max_socket < 0) {
 		fprintf(stderr, "Error finding last socket\n");
 		return 0;
 	}
 
-	/* replace socket with a new one */
-	if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) < 0)
-		return 0;
-
-	tds_socket_set_nosigpipe(sockets[0], 1);
-
-	/* substitute socket */
-	close(max_socket);
-	dup2(sockets[0], max_socket);
-
 	/* close connection */
-	close(sockets[0]);
-	close(sockets[1]);
+	shutdown(max_socket, SHUT_RDWR);
+
 	return 1;
 }
 
@@ -111,14 +102,3 @@ main(void)
 		return 1;
 	return 0;
 }
-
-#else
-int
-main(void)
-{
-	printf("Not possible for this platform.\n");
-	odbc_test_skipped();
-	return 0;
-}
-#endif
-
