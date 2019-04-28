7339ddceacc5f03a907332e8acef033d3811791e
Frediano Ziglio <freddy77@gmail.com>
2019-04-28 19:30:11 +0100

    Fix endian adjustment on PPC little endian
    Code assumed PPC were always big endian.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- include/freetds/bytes.h
+++ include/freetds/bytes.h
@@ -199,7 +199,7 @@ typedef union {
    uint32_t _tds_i = bswap_32(val); TDS_PUT_UA4LE(ptr,_tds_i); } while(0)
 #endif
 
-#if defined(__GNUC__) && defined(__powerpc__)
+#if defined(__GNUC__) && defined(__powerpc__) && defined(WORDS_BIGENDIAN)
 # undef TDS_GET_UA2LE
 # undef TDS_GET_UA4LE
 static inline uint16_t
