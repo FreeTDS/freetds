32906a29be24ebfeb2875bbc5ff67a00da36f21a
Frediano Ziglio <freddy77@gmail.com>
2021-12-12 19:46:14 +0000

    defncopy: Supports varchar(max)/varbinary(max)
    If length is -1 it means varchar(max) or varbinary(max) types.
    
    This fixes https://github.com/FreeTDS/freetds/issues/432.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/apps/defncopy.c
+++ src/apps/defncopy.c
@@ -577,7 +577,10 @@ print_ddl(DBPROCESS *dbproc, PROCEDURE *procedure)
 			for (t = varytypenames; *t; t++) {
 				if (0 == strcasecmp(*t, ddl[i].type)) {
 					ltrim(rtrim(ddl[i].length));
-					ret = asprintf(&type, "%s(%s)", ddl[i].type, ddl[i].length);
+					if (strcmp(ddl[i].length, "-1") == 0)
+						ret = asprintf(&type, "%s(max)", ddl[i].type);
+					else
+						ret = asprintf(&type, "%s(%s)", ddl[i].type, ddl[i].length);
 					break;
 				}
 			}
