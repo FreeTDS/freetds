53e156d03a8a942839b625f980cbedec67d26e45
Frediano Ziglio <freddy77@gmail.com>
2022-11-06 12:44:08 +0000

    odbc: Return size from SQLGetDescField for integers
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/odbc.c
+++ src/odbc/odbc.c
@@ -2543,6 +2543,7 @@ ODBC_FUNC(SQLGetDescField, (P(SQLHDESC,hdesc), P(SQLSMALLINT,icol), P(SQLSMALLIN
 {
 	struct _drecord *drec;
 	SQLRETURN result = SQL_SUCCESS;
+	SQLINTEGER dummy_size;
 
 	ODBC_ENTER_HDESC;
 
@@ -2553,10 +2554,16 @@ ODBC_FUNC(SQLGetDescField, (P(SQLHDESC,hdesc), P(SQLSMALLINT,icol), P(SQLSMALLIN
 #define IOUT(type, src) do { \
 	/* trick warning if type wrong */ \
 	type *p_test = &src; p_test = p_test; \
-	*((type *)Value) = src; } while(0)
+	*((type *)Value) = src; \
+	*StringLength = sizeof(type); } while(0)
 #else
-#define IOUT(type, src) *((type *)Value) = src
+#define IOUT(type, src) do { \
+	*((type *)Value) = src; \
+	*StringLength = sizeof(type); } while(0)
 #endif
+
+	if (!StringLength)
+		StringLength = &dummy_size;
 
 	/* dont check column index for these */
 	switch (fDescType) {
