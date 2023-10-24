23a36731db0be36ac948a4fde647303ba9574a9e
Frediano Ziglio <freddy77@gmail.com>
2023-10-24 21:05:11 +0100

    ctlib: Factor out a check_last_message to reduce checks in errors test
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/ctlib/unittests/errors.c
+++ src/ctlib/unittests/errors.c
@@ -21,9 +21,9 @@
 ALL_TESTS
 
 static void
-report_wrong_error(void)
+report_wrong_error(int line)
 {
-	fprintf(stderr, "Wrong error type %d number %d (%#x)\n",
+	fprintf(stderr, "%d:Wrong error type %d number %d (%#x)\n", line,
 		ct_last_message.type, ct_last_message.number, ct_last_message.number);
 	exit(1);
 }
@@ -78,41 +78,49 @@ _check_fail(const char *name, CS_RETCODE ret, int line)
 } while(0)
 
 static void
+_check_last_message(ct_message_type type, CS_INT number, const char *msg, int line)
+{
+	bool type_ok = true, number_ok = true, msg_ok = true;
+
+	if (type == CTMSG_NONE && ct_last_message.type == type)
+		return;
+
+	type_ok = (ct_last_message.type == type);
+	number_ok = (ct_last_message.number == number);
+	if (msg && msg[0])
+		msg_ok = (strstr(ct_last_message.text, msg) != NULL);
+	if (!type_ok || !number_ok || !msg_ok)
+		report_wrong_error(line);
+}
+#define check_last_message(type, number, msg) \
+	_check_last_message(type, number, msg, __LINE__)
+
+static void
 test_ct_callback(void)
 {
 	/* this should fail, context or connection should be not NULL */
 	check_fail(ct_callback, (NULL, NULL, CS_SET, CS_SERVERMSG_CB, servermsg_cb));
-	if (ct_last_message.type != CTMSG_NONE)
-		report_wrong_error();
+	check_last_message(CTMSG_NONE, 0, NULL);
 
 	/* this should fail, context and connection cannot be both not NULL */
 	check_fail(ct_callback, (ctx, conn, CS_SET, CS_SERVERMSG_CB, servermsg_cb));
-	if (ct_last_message.type != CTMSG_CLIENT2 || ct_last_message.number != 0x01010133)
-		report_wrong_error();
+	check_last_message(CTMSG_CLIENT2, 0x01010133, NULL);
 
 	/* this should fail, invalid action */
 	check_fail(ct_callback, (ctx, NULL, 3, CS_SERVERMSG_CB, servermsg_cb));
-	if (ct_last_message.type != CTMSG_CLIENT || ct_last_message.number != 0x01010105
-	    || !strstr(ct_last_message.text, "action"))
-		report_wrong_error();
+	check_last_message(CTMSG_CLIENT, 0x01010105, "action");
 
 	/* this should fail, invalid action */
 	check_fail(ct_callback, (NULL, conn, 3, CS_SERVERMSG_CB, servermsg_cb));
-	if (ct_last_message.type != CTMSG_CLIENT2 || ct_last_message.number != 0x01010105
-	    || !strstr(ct_last_message.text, "action"))
-		report_wrong_error();
+	check_last_message(CTMSG_CLIENT2, 0x01010105, "action");
 
 	/* this should fail, invalid type */
 	check_fail(ct_callback, (ctx, NULL, CS_SET, 20, servermsg_cb));
-	if (ct_last_message.type != CTMSG_CLIENT || ct_last_message.number != 0x01010105
-	    || !strstr(ct_last_message.text, "type"))
-		report_wrong_error();
+	check_last_message(CTMSG_CLIENT, 0x01010105, "type");
 
 	/* this should fail, invalid type */
 	check_fail(ct_callback, (NULL, conn, CS_SET, 20, servermsg_cb));
-	if (ct_last_message.type != CTMSG_CLIENT2 || ct_last_message.number != 0x01010105
-	    || !strstr(ct_last_message.text, "type"))
-		report_wrong_error();
+	check_last_message(CTMSG_CLIENT2, 0x01010105, "type");
 }
 
 static void
@@ -134,9 +142,7 @@ test_ct_res_info(void)
 		case CS_ROW_RESULT:
 			/* this should fail, invalid number */
 			check_fail(ct_res_info, (cmd, 1234, &num_cols, CS_UNUSED, NULL));
-			if (ct_last_message.type != CTMSG_CLIENT2 || ct_last_message.number != 0x01010105
-			    || !strstr(ct_last_message.text, "operation"))
-				report_wrong_error();
+			check_last_message(CTMSG_CLIENT2, 0x01010105, "operation");
 
 			while ((ret = ct_fetch(cmd, CS_UNUSED, CS_UNUSED, CS_UNUSED, &count)) == CS_SUCCEED)
 				continue;
@@ -166,9 +172,7 @@ test_ct_send(void)
 
 	/* this should fail, invalid command state */
 	check_fail(ct_send, (cmd));
-	if (ct_last_message.type != CTMSG_CLIENT2 || ct_last_message.number != 0x0101019b
-	    || !strstr(ct_last_message.text, "idle"))
-		report_wrong_error();
+	check_last_message(CTMSG_CLIENT2, 0x0101019b, "idle");
 }
 
 static void
@@ -210,22 +214,16 @@ test_cs_config(void)
 
 	for (p_invalid = invalid_values; *p_invalid != 0; ++p_invalid) {
 		check_fail(cs_config, (ctx, CS_SET, CS_USERDATA, (CS_VOID *)"test", *p_invalid, NULL));
-		if (ct_last_message.type != CTMSG_CSLIB || ct_last_message.number != 0x02010106
-		    || !strstr(ct_last_message.text, "buflen"))
-			report_wrong_error();
+		check_last_message(CTMSG_CSLIB, 0x02010106, "buflen");
 	}
 
 	/* wrong action */
 	check_fail(cs_config, (ctx, 1000, CS_USERDATA, (CS_VOID *)"test", 4, NULL));
-	if (ct_last_message.type != CTMSG_CSLIB || ct_last_message.number != 0x02010106
-	    || !strstr(ct_last_message.text, "action"))
-		report_wrong_error();
+	check_last_message(CTMSG_CSLIB, 0x02010106, "action");
 
 	/* wrong property */
 	check_fail(cs_config, (ctx, CS_SET, 100000, NULL, CS_UNUSED, NULL));
-	if (ct_last_message.type != CTMSG_CSLIB || ct_last_message.number != 0x02010106
-	    || !strstr(ct_last_message.text, "property"))
-		report_wrong_error();
+	check_last_message(CTMSG_CSLIB, 0x02010106, "property");
 
 	/* read exactly expected bytes */
 	check_call(cs_config, (ctx, CS_GET, CS_USERDATA, (CS_VOID *)out_buf, 4, NULL));
@@ -233,9 +231,7 @@ test_cs_config(void)
 	/* wrong buflen getting value */
 	out_len = -123;
 	check_fail(cs_config, (ctx, CS_GET, CS_USERDATA, (CS_VOID *)out_buf, CS_NULLTERM, &out_len));
-	if (ct_last_message.type != CTMSG_CSLIB || ct_last_message.number != 0x02010106
-	    || !strstr(ct_last_message.text, "buflen"))
-		report_wrong_error();
+	check_last_message(CTMSG_CSLIB, 0x02010106, "buflen");
 	if (out_len != -123) {
 		fprintf(stderr, "Wrong buffer length returned\n");
 		exit(1);
@@ -245,9 +241,7 @@ test_cs_config(void)
 	out_len = -123;
 	strcpy(out_buf, "123456");
 	check_fail(cs_config, (ctx, CS_GET, CS_USERDATA, (CS_VOID *)out_buf, 2, &out_len));
-	if (ct_last_message.type != CTMSG_CSLIB || ct_last_message.number != 0x02010102
-	    || !strstr(ct_last_message.text, " 2 bytes"))
-		report_wrong_error();
+	check_last_message(CTMSG_CSLIB, 0x02010102, " 2 bytes");
 	if (out_len != 4) {
 		fprintf(stderr, "Wrong buffer length returned\n");
 		exit(1);
