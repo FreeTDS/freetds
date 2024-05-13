6facddb97f4ea071f4002a5e2d22d8b9a5897853
Aaron M. Ucko <ucko@ncbi.nlm.nih.gov>
2024-05-13 14:24:07 -0400

    Implement blk_textxfer, adjusting libtds as needed.
    * tds.h (struct tds_bcpinfo): Add more fields to enable the necessary
      bookkeeping.
    * ctlib/blk.c: Implement blk_textfer; adjust other functions accordingly.
    * tds/{bulk,data}.c: Allow for deferred, and possibly piecemeal, blob
      submission; in particular, make tds_bcp_send_record resumable.
    
    Signed-off-by: Aaron M. Ucko <ucko@ncbi.nlm.nih.gov>

--- src/ctlib/blk.c
+++ src/ctlib/blk.c
@@ -318,6 +318,9 @@ static void _blk_clean_desc (CS_BLKDESC * blkdesc)
 	blkdesc->bcpinfo.direction = 0;
 	blkdesc->bcpinfo.bind_count = CS_UNUSED;
 	blkdesc->bcpinfo.xfer_init = 0;
+	blkdesc->bcpinfo.text_sent = 0;
+	blkdesc->bcpinfo.next_col = 0;
+	blkdesc->bcpinfo.blob_cols = 0;
 }
 
 CS_RETCODE
@@ -519,10 +522,36 @@ blk_srvinit(SRV_PROC * srvproc, CS_BLKDESC * blkdescp)
 CS_RETCODE
 blk_textxfer(CS_BLKDESC * blkdesc, CS_BYTE * buffer, CS_INT buflen, CS_INT * outlen)
 {
-	tdsdump_log(TDS_DBG_FUNC, "blk_textxfer(%p, %p, %d, %p)\n", blkdesc, buffer, buflen, outlen);
+	TDSSOCKET *tds;
+	TDSCOLUMN *bindcol;
 
-	tdsdump_log(TDS_DBG_FUNC, "UNIMPLEMENTED blk_textxfer()\n");
-	return CS_FAIL;
+	if (blkdesc == NULL  ||	 buffer == NULL) {
+		return CS_FAIL;
+	}
+
+	tds = CONN(blkdesc)->tds_socket;
+
+	bindcol = blkdesc->bcpinfo.bindinfo->columns
+		[blkdesc->bcpinfo.next_col-1];
+
+	if (bindcol->column_varaddr != NULL) {
+		return CS_FAIL;
+	}
+
+	bindcol->column_cur_size = buflen;
+	bindcol->column_lenbind = &bindcol->column_cur_size;
+	bindcol->column_varaddr = (TDS_CHAR*) buffer;
+
+	if (TDS_FAILED(tds_bcp_send_record(tds, &blkdesc->bcpinfo,
+					   _blk_get_col_data, _blk_null_error,
+					   0))) {
+		return CS_FAIL;
+	} else if (blkdesc->bcpinfo.next_col == 0) {
+		return CS_END_DATA; /* all done */
+	} else {
+		bindcol->column_varaddr = NULL;
+		return CS_SUCCEED; /* still need more data */
+	}
 }
 
 static CS_RETCODE
@@ -619,6 +648,8 @@ _blk_rowxfer_in(CS_BLKDESC * blkdesc, CS_INT rows_to_xfer, CS_INT * rows_xferred
 
 	if (blkdesc->bcpinfo.xfer_init == 0) {
 
+		blkdesc->bcpinfo.xfer_init = 1;
+
 		/*
 		 * first call the start_copy function, which will
 		 * retrieve details of the database table columns
@@ -626,16 +657,19 @@ _blk_rowxfer_in(CS_BLKDESC * blkdesc, CS_INT rows_to_xfer, CS_INT * rows_xferred
 
 		if (TDS_FAILED(tds_bcp_start_copy_in(tds, &blkdesc->bcpinfo))) {
 			_ctclient_msg(NULL, CONN(blkdesc), "blk_rowxfer", 2, 5, 1, 140, "");
+
+			blkdesc->bcpinfo.xfer_init = 0;
 			return CS_FAIL;
 		}
-
-		blkdesc->bcpinfo.xfer_init = 1;
 	} 
 
 	for (each_row = 0; each_row < rows_to_xfer; each_row++ ) {
 
 		if (tds_bcp_send_record(tds, &blkdesc->bcpinfo, _blk_get_col_data, _blk_null_error, each_row) == TDS_SUCCESS) {
-			/* FIXME */
+			if (blkdesc->bcpinfo.next_col > 0)
+				return CS_BLK_HAS_TEXT;
+		} else {
+			return CS_FAIL;
 		}
 	}
 
@@ -670,19 +704,6 @@ _blk_get_col_data(TDSBCPINFO *bulk, TDSCOLUMN *bindcol, int offset)
 
 	tdsdump_log(TDS_DBG_FUNC, "_blk_get_col_data(%p, %p, %d)\n", bulk, bindcol, offset);
 
-	/*
-	 * Retrieve the initial bound column_varaddress
-	 * and increment it if offset specified
-	 */
-
-	src = (unsigned char *) bindcol->column_varaddr;
-	if (!src) {
-		tdsdump_log(TDS_DBG_ERROR, "error source field not addressable\n");
-		return TDS_FAIL;
-	}
-
-	src += offset * bindcol->column_bindlen;
-	
 	if (bindcol->column_nullbind) {
 		nullind = bindcol->column_nullbind;
 		nullind += offset;
@@ -692,6 +713,31 @@ _blk_get_col_data(TDSBCPINFO *bulk, TDSCOLUMN *bindcol, int offset)
 		datalen += offset;
 	}
 
+	/*
+	 * Retrieve the initial bound column_varaddress
+	 * and increment it if offset specified
+	 */
+
+	src = (unsigned char *) bindcol->column_varaddr;
+	if (!src) {
+		if (nullind  &&	 *nullind == -1) {
+			null_column = true;
+		}
+
+		bindcol->bcp_column_data->datalen = *datalen;
+		bindcol->bcp_column_data->is_null = null_column;
+
+		if (is_blob_col(bindcol)
+		    &&	bindcol->column_varaddr == NULL) {
+			/* Data will come piecemeal, via blk_textxfer. */
+			return TDS_NO_MORE_RESULTS;
+		}
+
+		tdsdump_log(TDS_DBG_ERROR, "error source field not addressable\n");
+		return TDS_FAIL;
+	}
+
+	src += offset * bindcol->column_bindlen;
 	srctype = bindcol->column_bindtype; 		/* passes to cs_convert */
 
 	tdsdump_log(TDS_DBG_INFO1, "blk_get_col_data srctype = %d\n", srctype);
@@ -731,7 +777,7 @@ _blk_get_col_data(TDSBCPINFO *bulk, TDSCOLUMN *bindcol, int offset)
 			null_column = true;
 	}
 
-	if (!null_column) {
+	if (!null_column  &&  !is_blob_type(bindcol->column_type)) {
 		CONV_RESULT convert_buffer;
 		CS_DATAFMT_COMMON srcfmt, destfmt;
 		CS_INT desttype;
