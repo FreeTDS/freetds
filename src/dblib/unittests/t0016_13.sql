300611313f0e6d683bce436ac6b81df71265e4a2
Frediano Ziglio <freddy77@gmail.com>
2024-06-01 18:14:50 +0100

    Add test for nvarchar(max) and bulk copy
    Data include empty and NULL data.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

---
+++ src/dblib/unittests/t0016_13.sql
@@ -0,0 +1,8 @@
+create table #dblib0016 (id int not null, var uniqueidentifier null, c1 nvarchar(max) null)
+go
+select * from #dblib0016 where 0=1
+go
+select * from #dblib0016 where 0=1
+go
+drop table #dblib0016
+go
