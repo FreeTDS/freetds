cd6800061578a3c131eb270555edb53e90a0138a
Frediano Ziglio <freddy77@gmail.com>
2024-07-18 17:18:24 +0100

    odbc: Fix typo in comment
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/unittests/closestmt.c
+++ src/odbc/unittests/closestmt.c
@@ -1,7 +1,7 @@
 #include "common.h"
 
 /*
- * This test attempt to test if closing a statement with prepared query
+ * This test attempts to test if closing a statement with prepared query
  * success if there are a pending query on the same connection from
  * another statement.
  */
