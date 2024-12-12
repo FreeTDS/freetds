9694e27993fa6fdf3e7dba5c18ca3a87cdb48d3d
Frediano Ziglio <freddy77@gmail.com>
2024-12-12 21:23:33 +0000

    odbc: Reduce code duplication in insert_speed test
    insert_test_auto and insert_test_man were almost identical.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/unittests/insert_speed.c
+++ src/odbc/unittests/insert_speed.c
@@ -8,38 +8,16 @@
 /* I don't remember where this test came ... - freddy77 */
 
 static void
-insert_test_auto(void)
+insert_test(bool manual)
 {
 	SQLLEN sql_nts = SQL_NTS;
 	SQLINTEGER id = 0;
 	char string[64];
-
-	odbc_reset_statement();
-
-	CHKBindParameter(1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_INTEGER, sizeof(id), 0, &id, 0, &sql_nts, "SI");
-	CHKBindParameter(2, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_VARCHAR, sizeof(string), 0, string, 0, &sql_nts, "SI");
-
-	CHKPrepare(T("insert into test values (?, ?)"), SQL_NTS, "SI");
-	for (id = 0; id < 20; id++) {
-		sprintf(string, "This is a test (%d)", (int) id);
-		CHKExecute("SI");
-	}
-
-	odbc_reset_statement();
-}
-
-
-static void
-insert_test_man(void)
-{
-	SQLLEN sql_nts = SQL_NTS;
 	SQLINTEGER commit_off = SQL_AUTOCOMMIT_OFF;
 	SQLINTEGER commit_on = SQL_AUTOCOMMIT_ON;
-	SQLINTEGER id = 0;
 
-	char string[64];
-
-	CHKSetConnectAttr(SQL_ATTR_AUTOCOMMIT, TDS_INT2PTR(commit_off), SQL_IS_INTEGER, "SI");
+	if (manual)
+		CHKSetConnectAttr(SQL_ATTR_AUTOCOMMIT, TDS_INT2PTR(commit_off), SQL_IS_INTEGER, "SI");
 
 	odbc_reset_statement();
 
@@ -52,8 +30,10 @@ insert_test_man(void)
 		CHKExecute("SI");
 	}
 
-	SQLEndTran(SQL_HANDLE_DBC, odbc_conn, SQL_COMMIT);
-	SQLSetConnectAttr(odbc_conn, SQL_ATTR_AUTOCOMMIT, TDS_INT2PTR(commit_on), SQL_IS_INTEGER);
+	if (manual) {
+		SQLEndTran(SQL_HANDLE_DBC, odbc_conn, SQL_COMMIT);
+		SQLSetConnectAttr(odbc_conn, SQL_ATTR_AUTOCOMMIT, TDS_INT2PTR(commit_on), SQL_IS_INTEGER);
+	}
 	odbc_reset_statement();
 }
 
@@ -65,11 +45,13 @@ main(void)
 	odbc_command_with_result(odbc_stmt, "DROP TABLE test");
 	odbc_command("CREATE TABLE test(i int, c varchar(40))");
 
-	insert_test_man();
+	/* manual commit */
+	insert_test(true);
 
 	odbc_command("DELETE FROM test");
 
-	insert_test_auto();
+	/* auto commit */
+	insert_test(false);
 
 	odbc_command("DROP TABLE test");
 
