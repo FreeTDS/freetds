7ea1a54a744a9138f5ae9f259fd25537cc0141e6
Matt McNabb <Matthew.mcnabb@vmssoftware.com>
2025-11-07 15:24:15 +1300

    FreeBCP: mark private functions as static, and match prototype to definition
    Signed-off-by: Matt McNabb <Matthew.mcnabb@vmssoftware.com>

--- src/apps/freebcp.c
+++ src/apps/freebcp.c
@@ -62,19 +62,20 @@
 #include <sybdb.h>
 #include "freebcp.h"
 
-void pusage(void);
-int process_parameters(int, char **, struct pd *);
+static void pusage(void);
+static int process_parameters(int, char **, BCPPARAMDATA *);
 static int unescape(char arg[]);
-int login_to_database(struct pd *, DBPROCESS **);
+static int login_to_database(BCPPARAMDATA * pdata, DBPROCESS ** pdbproc);
 
-int file_character(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir);
-int file_native(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir);
-int file_formatted(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir);
-int setoptions (DBPROCESS * dbproc, BCPPARAMDATA * params);
+static int file_character(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir);
+static int file_native(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir);
+static int file_formatted(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir);
+static int setoptions (DBPROCESS * dbproc, BCPPARAMDATA * params);
 
-int err_handler(DBPROCESS * dbproc, int severity, int dberr, int oserr, char *dberrstr, char *oserrstr);
-int msg_handler(DBPROCESS * dbproc, DBINT msgno, int msgstate, int severity, char *msgtext, char *srvname, char *procname,
+static int err_handler(DBPROCESS * dbproc, int severity, int dberr, int oserr, char *dberrstr, char *oserrstr);
+static int msg_handler(DBPROCESS * dbproc TDS_UNUSED, DBINT msgno, int msgstate, int severity, char *msgtext, char *srvname, char *procname,
 		int line);
+
 static int set_bcp_hints(BCPPARAMDATA *pdata, DBPROCESS *pdbproc);
 
 int
@@ -162,7 +163,7 @@ static int unescape(char arg[])
 	return strchr(p, 0) - arg;
 }
 
-int
+static int
 process_parameters(int argc, char **argv, BCPPARAMDATA *pdata)
 {
 	extern char *optarg;
@@ -403,7 +404,7 @@ process_Eflag(BCPPARAMDATA* pdata, DBPROCESS* dbproc)
 	return TRUE;
 }
 
-int
+static int
 login_to_database(BCPPARAMDATA * pdata, DBPROCESS ** pdbproc)
 {
 	LOGINREC *login;
@@ -472,7 +473,7 @@ login_to_database(BCPPARAMDATA * pdata, DBPROCESS ** pdbproc)
 
 }
 
-int
+static int
 file_character(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir)
 {
 	DBINT li_rowsread = 0;
@@ -521,7 +522,7 @@ file_character(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir)
 	return TRUE;
 }
 
-int
+static int
 file_native(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir)
 {
 	DBINT li_rowsread = 0;
@@ -568,7 +569,7 @@ file_native(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir)
 	return TRUE;
 }
 
-int
+static int
 file_formatted(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir)
 {
 
@@ -603,7 +604,7 @@ file_formatted(BCPPARAMDATA * pdata, DBPROCESS * dbproc, DBINT dir)
 }
 
 
-int
+static int
 setoptions(DBPROCESS * dbproc, BCPPARAMDATA * params)
 {
 	RETCODE fOK;
@@ -678,7 +679,7 @@ set_bcp_hints(BCPPARAMDATA *pdata, DBPROCESS *pdbproc)
 	return TRUE;
 }
 
-void
+static void
 pusage(void)
 {
 	fprintf(stderr, "usage:  freebcp [[database_name.]owner.]table_name|query {in | out | queryout } datafile\n");
@@ -693,7 +694,7 @@ pusage(void)
 	fprintf(stderr, "example: freebcp testdb.dbo.inserttest in inserttest.txt -S mssql -U guest -P password -c\n");
 }
 
-int
+static int
 err_handler(DBPROCESS * dbproc, int severity, int dberr, int oserr TDS_UNUSED, char *dberrstr, char *oserrstr TDS_UNUSED)
 {
 	static int sent = 0;
@@ -715,7 +716,7 @@ err_handler(DBPROCESS * dbproc, int severity, int dberr, int oserr TDS_UNUSED, c
 	return INT_CANCEL;
 }
 
-int
+static int
 msg_handler(DBPROCESS * dbproc TDS_UNUSED, DBINT msgno, int msgstate, int severity,
 	    char *msgtext, char *srvname, char *procname, int line)
 {
