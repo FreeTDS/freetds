8b14f0fe9283b42cf5117cb34a164754050a96f4
Frediano Ziglio <freddy77@gmail.com>
2025-11-09 21:24:50 +0000

    cmake: Use "len" result in source code checks
    This potentially avoid some warning compiling the code.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- misc/cmake_checks.c
+++ misc/cmake_checks.c
@@ -4,22 +4,23 @@
 
 int main(int argc, char **argv)
 {
+  int len;
 #if defined(CHECK_ASPRINTF)
-  char *p = NULL; int len = asprintf(&p, "%d", 123);
+  char *p = NULL; len = asprintf(&p, "%d", 123);
 #elif defined(CHECK_VASPRINTF)
-  va_list va; char *p = NULL; int len = vasprintf(&p, "%d", va);
+  va_list va; char *p = NULL; len = vasprintf(&p, "%d", va);
 #elif defined(CHECK_SNPRINTF)
-  char buf[128]; int len = snprintf(buf, 128, "%d", 123);
+  char buf[128]; len = snprintf(buf, 128, "%d", 123);
 #elif defined(CHECK__SNPRINTF)
-  char buf[128]; int len = _snprintf(buf, 128, "%d", 123);
+  char buf[128]; len = _snprintf(buf, 128, "%d", 123);
 #elif defined(CHECK_VSNPRINTF)
-  va_list va; char buf[128]; int len = vsnprintf(buf, 128, "%d", va);
+  va_list va; char buf[128]; len = vsnprintf(buf, 128, "%d", va);
 #elif defined(CHECK__VSNPRINTF)
-  va_list va; char buf[128]; int len = _vsnprintf(buf, 128, "%d", va);
+  va_list va; char buf[128]; len = _vsnprintf(buf, 128, "%d", va);
 #elif defined(CHECK__VSCPRINTF)
-  va_list va; int len = _vscprintf("%d", va);
+  va_list va; len = _vscprintf("%d", va);
 #else
 #error "Check function not specified correctly"
 #endif
-  return 0;
+  return !!len;
 }
