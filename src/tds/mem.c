4c04df68201bca0b568174952007e856d9c6490f
Matt McNabb <matthew.mcnabb@vmssoftware.com>
2025-12-15 01:58:20 -0500

    bcp in: Uninitialized columns should be NULL, not zero-length value
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

--- src/tds/mem.c
+++ src/tds/mem.c
@@ -1831,7 +1831,13 @@ tds_alloc_bcp_column_data(unsigned int column_size)
 {
 	BCPCOLDATA *coldata;
 
-	TEST_MALLOC(coldata, BCPCOLDATA);
+    /* Initialize fields to NULL, otherwise any field with value not
+     * explicitly set will behave as a non-null value of length 0,
+     * which is not really what we ever want.
+     * ASE BCP treats uninitialized fields as null.
+     */
+	TEST_CALLOC(coldata, BCPCOLDATA, 1);
+    coldata->is_null = true;
 
 	if (column_size > 4 * 1024)
 		column_size = 4 * 1024;
