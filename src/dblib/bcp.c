88d0b1fc933b7604bc409f4fa11de400fb1c7fa6
Matt McNabb <Matthew.mcnabb@vmssoftware.com>
2026-02-13 14:03:29 +1300

    bcp in: Avoid calling ftell in hot loop
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

--- src/dblib/bcp.c
+++ src/dblib/bcp.c
@@ -1165,7 +1165,7 @@ _bcp_read_hostfile(DBPROCESS * dbproc, TDSFILESTREAM *stream, bool *row_error, b
 		TDS_CHAR *coldata;
 		int collen = 0;
 		bool data_is_null = false;
-		offset_type col_start;
+		offset_type col_start = 0;
 
 		tdsdump_log(TDS_DBG_FUNC, "parsing host column %d\n", i + 1);
 		hostcol = dbproc->hostfileinfo->host_columns[i];
@@ -1249,8 +1249,10 @@ _bcp_read_hostfile(DBPROCESS * dbproc, TDSFILESTREAM *stream, bool *row_error, b
 		if (is_fixed_type(hostcol->datatype))
 			collen = tds_get_size_by_type(hostcol->datatype);
 
+#if ENABLE_EXTRA_CHECKS
+		/* Expensive operation for a hot loop */
 		col_start = tds_file_stream_tell(stream);
-
+#endif
 		/*
 		 * The data file either contains prefixes stating the length, or is delimited.  
 		 * If delimited, we "measure" the field by looking for the terminator, then read it, 
@@ -1346,8 +1348,10 @@ _bcp_read_hostfile(DBPROCESS * dbproc, TDSFILESTREAM *stream, bool *row_error, b
 				if (TDS_FAILED(rc)) {
 					hostcol->column_error = HOST_COL_CONV_ERROR;
 					*row_error = true;
+					if ( col_start == 0 )
+						col_start = tds_file_stream_tell(stream) - collen;
 					tdsdump_log(TDS_DBG_FUNC, 
-						"_bcp_read_hostfile failed to convert %d bytes at offset 0x%" PRIx64 " in the data file.\n",
+						"_bcp_read_hostfile failed to convert %d bytes at offset about 0x%" PRIx64 " in the data file.\n",
 						    collen, (TDS_INT8) col_start);
 				}
 
