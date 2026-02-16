7ebec7613f29267ce3675a0dd3109a498f4b70f1
Matt McNabb <matthew.mcnabb@vmssoftware.com>
2026-02-16 13:39:43 +1300

    t0016: defaults test can run against both database types. Include datetime
    	MSSQL does allow NULL to be sent for non-nullable VARCHAR.
    
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

--- src/dblib/unittests/t0016_17.sql
+++ src/dblib/unittests/t0016_17.sql
@@ -1,5 +1,9 @@
--- test default values for Sybase
-create table #dblib0016 (id int not null, test1 varchar(20) default 'hello', test2 varchar(20) null, test3 unsigned int default 854)
+-- test default values for Sybase & MSSQL
+-- note: TDS protocol doesn't allow NULL to be encoded for non-nullable
+-- fixed fields; however VARCHAR NOT NULL fields can still have NULL sent.
+-- Sybase server would reject; MS server applies the default.
+-- (Sybase expects the client to implement default values; MS is all serverside)
+create table #dblib0016 (id int not null, test1 varchar(20) default 'hello' not null, test2 varchar(20) null, test3 int default 854 null, dt datetime default '2026-02-27 02:34:56.700' null)
 go
 select * from #dblib0016 where 0=1
 go
