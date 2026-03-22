45fedb0a40067445cff2d706098ffbe749f5e55d
Matt McNabb <matthew.mcnabb@vmssoftware.com>
2026-03-23 12:10:07 +1300

    Expand o_date test
    	- Test MSDATETIME2 full precision display.
    	- Test customized format strings.
    	- Check display size is not exceeded.
    	- Exit cleanly on test failure.
    
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

--- src/odbc/unittests/date.c
+++ src/odbc/unittests/date.c
@@ -1,6 +1,7 @@
 #include "common.h"
+#include <odbcss.h>
 
-static void
+static int
 DoTest(int n)
 {
 	SQLCHAR output[256];
@@ -9,14 +10,51 @@ DoTest(int n)
 	SQLULEN colSize;
 	SQLSMALLINT colScale, colNullable;
 	SQLLEN dataSize;
+	char const* expect;
 
 	TIMESTAMP_STRUCT ts;
 
-	odbc_command("select convert(datetime, '2002-12-27 18:43:21')");
+	switch (n)
+	{
+	case 0:
+	case 1:
+		odbc_command("select convert(datetime, '2002-12-27 18:43:21')");
+		expect = "2002-12-27 18:43:21.000";
+		break;
+
+	case 2:
+		/* Recent feature added to trunk - Full precision for datetime2 */
+		odbc_command("select convert(datetime2, '2002-12-27 18:43:21')");
+		expect = "2002-12-27 18:43:21.0000000";
+		break;
+
+	case 3:
+		SQLSetConnectAttr(odbc_conn, SQL_COPT_TDSODBC_DATETIME_FORMAT, T("%d-%m-%Y %H:%M:%S"), SQL_NTS);
+		odbc_command("select convert(datetime, '2002-12-27 18:43:21')");
+		expect = "27-12-2002 18:43:21";
+		break;
+
+	case 4:
+		SQLSetConnectAttr(odbc_conn, SQL_COPT_TDSODBC_DATE_FORMAT, T("**%d-%m-%Y**"), SQL_NTS);
+		odbc_command("select convert(date, '2002-12-27 18:43:21')");
+		expect = "**27-12-2002**";
+		break;
+
+	case 5:
+		SQLSetConnectAttr(odbc_conn, SQL_COPT_TDSODBC_TIME_FORMAT, T("**%H:%M:%S**"), SQL_NTS);
+		odbc_command("select convert(time, '2002-12-27 18:43:21')");
+		expect = "**18:43:21**";
+		break;
+
+	default:
+		printf("Done.\n");
+		return 0;
+	}
 
 	CHKFetch("SI");
 	CHKDescribeCol(1, (SQLTCHAR*)output, sizeof(output)/sizeof(SQLWCHAR), NULL, &colType, &colSize, &colScale, &colNullable, "S");
 
+	/* Case 0 tests binding SQL_C_TIMESTAMP to result; other cases are testing binding to char. */
 	if (n == 0) {
 		memset(&ts, 0, sizeof(ts));
 		CHKGetData(1, SQL_C_TIMESTAMP, &ts, sizeof(ts), &dataSize, "S");
@@ -25,25 +63,29 @@ DoTest(int n)
 		CHKGetData(1, SQL_C_CHAR, output, sizeof(output), &dataSize, "S");
 	}
 
-	printf("Date returned: %s\n", output);
-	if (strcmp((char *) output, "2002-12-27 18:43:21.000") != 0) {
-		fprintf(stderr, "Invalid returned date\n");
-		exit(1);
+	printf("Date returned: \"%s\"\n", output);
+	if (strcmp((char *) output, expect) != 0) {
+		fprintf(stderr, "Invalid returned date; expected \"%s\".\n", expect);
+		return 1;
 	}
 
+	if ((int)colSize < dataSize)
+	{
+		fprintf(stderr, "Column described as size %d, but data length was %d\n", (int)colSize, (int)dataSize);
+		return 1;
+	}
 	CHKFetch("No");
 	CHKCloseCursor("SI");
+	return -1;
 }
 
 TEST_MAIN()
 {
+	int i = 0;
+	int exit_status;
 	odbc_connect();
-
-	DoTest(0);
-	DoTest(1);
-
+	while ((exit_status = DoTest(i++)) == -1) {}
 	odbc_disconnect();
 
-	printf("Done.\n");
-	return 0;
+	return exit_status;
 }
