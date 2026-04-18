d7d4ff7d7e4ef16c5cde6598569851374ba832b1
Frediano Ziglio <freddy77@gmail.com>
2026-04-18 09:31:04 +0100

    odbc: Check ARD allocation
    Check cases when ARD records are allocated.
    Tested also with MS driver.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/unittests/descrec.c
+++ src/odbc/unittests/descrec.c
@@ -2,6 +2,18 @@
 #include "common.h"
 
 static void
+check_int(bool cond, long int value, const char *msg, int line)
+{
+	if (cond)
+		return;
+	fprintf(stderr, "Invalid value %ld at line %d, check: %s\n", value, line, msg);
+	exit(1);
+}
+
+#define check_int(value, cond, expected) \
+	check_int(value cond expected, value, #value " " #cond " " #expected, __LINE__)
+
+static void
 test_column_number(void)
 {
 	SQLHDESC Descriptor;
@@ -29,6 +41,33 @@ test_column_number(void)
 		      NULL /*Precision */ , NULL /*Scale */ , NULL /*Nullable */ , "S");
 }
 
+static void
+test_ard_allocation(void)
+{
+	SQLHDESC ard;
+	SQLINTEGER ind;
+	SQLSMALLINT count;
+
+	odbc_reset_statement();
+
+	CHKGetStmtAttr(SQL_ATTR_APP_ROW_DESC, &ard, sizeof(ard), &ind, "S");
+
+	/* A query should not extent ARD */
+	odbc_command("SELECT name FROM #tmp1");
+	CHKGetDescField(ard, 0, SQL_DESC_COUNT, &count, sizeof(count), &ind, "S");
+	check_int(count, ==, 0);
+
+	/* This should not extent ARD */
+	CHKSetDescField(ard, 2, SQL_DESC_AUTO_UNIQUE_VALUE, TDS_INT2PTR(10), 0, "E");
+	CHKGetDescField(ard, 0, SQL_DESC_COUNT, &count, sizeof(count), &ind, "S");
+	check_int(count, ==, 0);
+
+	/* This should extent ARD */
+	CHKSetDescField(ard, 2, SQL_DESC_SCALE, TDS_INT2PTR(10), 0, "S");
+	CHKGetDescField(ard, 0, SQL_DESC_COUNT, &count, sizeof(count), &ind, "S");
+	check_int(count, ==, 2);
+}
+
 TEST_MAIN()
 {
 	odbc_use_version3 = true;
@@ -37,6 +76,7 @@ TEST_MAIN()
 	odbc_command("create table #tmp1 (name varchar(100))");
 
 	test_column_number();
+	test_ard_allocation();
 
 	odbc_disconnect();
 	ODBC_FREE();
