2bdeff9407c3a235da6fd6dce0995118307c4812
Frediano Ziglio <freddy77@gmail.com>
2026-04-18 08:00:04 +0100

    odbc: Separate test case for descrec test
    Prepare to expand the test.
    
    Signed-off-by: Frediano Ziglio <freddy77@gmail.com>

--- src/odbc/unittests/descrec.c
+++ src/odbc/unittests/descrec.c
@@ -1,34 +1,44 @@
 /* test SQLGetDescRec */
 #include "common.h"
 
-TEST_MAIN()
+static void
+test_column_number(void)
 {
 	SQLHDESC Descriptor;
 	SQLINTEGER ind;
 	SQLTCHAR name[128];
 	SQLSMALLINT si;
 
-	odbc_use_version3 = true;
-	odbc_connect();
-
-	odbc_command("create table #tmp1 (i int)");
-
 	/* get IRD */
 	CHKGetStmtAttr(SQL_ATTR_IMP_ROW_DESC, &Descriptor, sizeof(Descriptor), &ind, "S");
 
+	/* Wrong column number */
 	CHKGetDescRec(-1, name, TDS_VECTOR_SIZE(name), &si, NULL, NULL, NULL, NULL, NULL, NULL, "E");
+
 	/* TODO here should be NO_DATA cause we are requesting bookmark */
 	/* CHKGetDescRec(0, name, sizeof(name), &si, NULL, NULL, NULL, NULL, NULL, NULL, "No"); */
-	CHKGetDescRec(1, name, TDS_VECTOR_SIZE(name), &si, NULL /*Type*/, NULL /*SubType*/, NULL /*Length*/, NULL/*Precision*/,
-		      NULL /*Scale*/, NULL /*Nullable*/, "No");
 
-	odbc_command("SELECT name FROM sysobjects");
+	/* No column present */
+	CHKGetDescRec(1, name, TDS_VECTOR_SIZE(name), &si, NULL /*Type */ , NULL /*SubType */ , NULL /*Length */ ,
+		      NULL /*Precision */ , NULL /*Scale */ , NULL /*Nullable */ , "No");
 
-	CHKGetDescRec(1, name, TDS_VECTOR_SIZE(name), &si, NULL /*Type*/, NULL /*SubType*/, NULL /*Length*/, NULL/*Precision*/,
-		      NULL /*Scale*/, NULL /*Nullable*/, "S");
+	odbc_command("SELECT name FROM #tmp1");
+
+	/* Column present */
+	CHKGetDescRec(1, name, TDS_VECTOR_SIZE(name), &si, NULL /*Type */ , NULL /*SubType */ , NULL /*Length */ ,
+		      NULL /*Precision */ , NULL /*Scale */ , NULL /*Nullable */ , "S");
+}
+
+TEST_MAIN()
+{
+	odbc_use_version3 = true;
+	odbc_connect();
+
+	odbc_command("create table #tmp1 (name varchar(100))");
+
+	test_column_number();
 
 	odbc_disconnect();
 	ODBC_FREE();
 	return 0;
 }
-
