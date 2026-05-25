33b4fda6a150f3357d60817dba21fb35355c16f7
Matt McNabb <matthew.mcnabb@vmssoftware.com>
2026-05-26 10:48:33 +1200

    t0016_22: test oversized length-prefixed data
    	Test file has a length-prefixed field (size 32+ so we
    	can have the prefix byte in the input file without
    	hex editing), then a delimited field. In both cases
    	data should truncate to server size.
    
    Signed-off-by: Matt McNabb <matthew.mcnabb@vmssoftware.com>

---
+++ src/dblib/unittests/t0016_22.sql
@@ -0,0 +1,8 @@
+create table #dblib0016 (f0 int not null, f1 varchar(32) not null, f2 varchar(20) not null, f3 int not null)
+go
+select * from #dblib0016 where 0=1
+go
+select * from #dblib0016 where 0=1
+go
+drop table #dblib0016
+go
